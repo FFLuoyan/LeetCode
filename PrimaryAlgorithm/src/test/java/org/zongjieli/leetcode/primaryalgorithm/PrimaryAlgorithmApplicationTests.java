3959fa9b49181abc46485f8223fe732850f1269c
LiZongjie <164260294@qq.com>
2020-01-03 14:26:13 +0800

    I don't know for the time being what this is using for

---
+++ PrimaryAlgorithm/src/test/java/org/zongjieli/leetcode/primaryalgorithm/PrimaryAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zongjieli.leetcode.primaryalgorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimaryAlgorithmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
