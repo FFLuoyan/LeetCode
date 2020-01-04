806e4db7f6dcbed9accd89ce952fef5dbdae6145
LiZongjie <164260294@qq.com>
2020-01-04 16:31:29 +0800

    optimize code

--- PrimaryAlgorithm/src/main/java/org/zongjieli/leetcode/primaryalgorithm/string/StringToInteger.java
+++ PrimaryAlgorithm/src/main/java/org/zongjieli/leetcode/primaryalgorithm/string/StringToInteger.java
@@ -10,59 +10,49 @@ package org.zongjieli.leetcode.primaryalgorithm.string;
 public class StringToInteger {
     public static int myAtoi(String str) {
         char[] strCharArr = str.toCharArray();
-        long numberResolve = 1L;
+        boolean isPositive = true;
         int resolveIndex = 0;
-        for (int getIndex = 0; getIndex < strCharArr.length; getIndex++) {
-            if (strCharArr[getIndex] != ' ') {
-                // if the character isn't blank,check positive or negative
-                if (strCharArr[getIndex] == '+') {
-                    resolveIndex = resolveNumber(strCharArr, getIndex + 1);
-                } else if (strCharArr[getIndex] == '-') {
-                    numberResolve = -1;
-                    resolveIndex = resolveNumber(strCharArr, getIndex + 1);
-                } else if (strCharArr[getIndex] >= '0' && strCharArr[getIndex] <= '9') {
-                    resolveIndex = resolveNumber(strCharArr, getIndex);
-                } else {
-                    //  can't resolve
-                    return 0;
-                }
-                //  once a char not blank is found,the number resolve start
-                //  and the loop is over
+        for (; resolveIndex < strCharArr.length; resolveIndex++) {
+            if (strCharArr[resolveIndex] == ' ') {
+                continue;
+            }
+            // if the character isn't blank,check positive or negative
+            if (strCharArr[resolveIndex] == '+') {
+                resolveIndex++;
                 break;
             }
-        }
-        if (resolveIndex == 0) {
+            if (strCharArr[resolveIndex] == '-') {
+                resolveIndex++;
+                isPositive = false;
+                break;
+            }
+            if (strCharArr[resolveIndex] >= '0' && strCharArr[resolveIndex] <= '9') {
+                break;
+            }
+            //  can't resolve
             return 0;
         }
-        if (resolveIndex > 10) {
-            return numberResolve > 0 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-        }
-        numberResolve = numberResolve * Long.valueOf(String.valueOf(strCharArr, 0, resolveIndex));
-        return numberResolve > Integer.MAX_VALUE ? Integer.MAX_VALUE : numberResolve < Integer.MIN_VALUE ? Integer.MIN_VALUE : (int) numberResolve;
-    }
-
-    public static int resolveNumber(char[] charArr, int startIndex) {
-        //  resolve number in char array;
-
         // trim zero
-        while (startIndex < charArr.length && charArr[startIndex] == '0') {
-            startIndex++;
+        while (resolveIndex < strCharArr.length && strCharArr[resolveIndex] == '0') {
+            resolveIndex++;
         }
-        int setIndex = 0;
-        while (startIndex < charArr.length) {
-            if (charArr[startIndex] >= '0' && charArr[startIndex] <= '9') {
-                //  set number string this character array
-                charArr[setIndex++] = charArr[startIndex];
-            } else {
-                //  the continued number is over
-                break;
+        long numberGet = 0;
+        long numberResolve = 0;
+        while (resolveIndex < strCharArr.length && strCharArr[resolveIndex] >= '0' && strCharArr[resolveIndex] <= '9') {
+            numberGet = numberGet * 10 + strCharArr[resolveIndex] - 48;
+            numberResolve = isPositive ? numberGet : -numberGet;
+            if (numberResolve > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
             }
-            startIndex++;
+            if (numberResolve < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+            resolveIndex++;
         }
-        return setIndex;
+        return (int) numberResolve;
     }
 
     public static void main(String[] args) {
-        System.out.println(myAtoi("-000000000000000001"));
+        System.out.println(myAtoi("    0004554"));
     }
 }
