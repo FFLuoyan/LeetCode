1902c04d87dc7be6d98b81bac9852db2f1162c33
LiZongjie <164260294@qq.com>
2020-01-10 13:49:27 +0800

    delete linked list node

---
+++ PrimaryAlgorithm/src/main/java/org/zongjieli/leetcode/primaryalgorithm/linkedlist/DeleteNode.java
@@ -0,0 +1,54 @@
+package org.zongjieli.leetcode.primaryalgorithm.linkedlist;
+
+/**
+ * @ClassName: DeleteNode
+ * @Description: TODO
+ * @Author: Zongjie.Li
+ * @Date: 2020/1/10
+ * @Version: 1.0
+ **/
+public class DeleteNode {
+    public static void deleteNode(ListNode node) {
+        while (node.next.next != null) {
+            node.val = node.next.val;
+            node = node.next;
+        }
+        node.val = node.next.val;
+        node.next = null;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(0);
+        ListNode currentNode = head;
+        ListNode nodeFive = null;
+        for (int i = 1; i < 10; i++) {
+            currentNode.next = new ListNode(i);
+            currentNode = currentNode.next;
+            if (currentNode.val == 5) {
+                nodeFive = currentNode;
+            }
+        }
+        System.out.println("Before Delete");
+        outputListNode(head);
+        deleteNode(nodeFive);
+        System.out.println("After Delete");
+        outputListNode(head);
+
+    }
+
+    public static void outputListNode(ListNode node) {
+        System.out.println(node.val);
+        if (node.next != null) {
+            outputListNode(node.next);
+        }
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
