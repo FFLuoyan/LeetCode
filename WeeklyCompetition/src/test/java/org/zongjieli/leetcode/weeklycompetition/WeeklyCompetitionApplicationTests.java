13dd8e046a132ae7074f989c38317005c0365a21
LiZongjie <164260294@qq.com>
2020-01-12 15:45:51 +0800

    weekly competition

---
+++ WeeklyCompetition/src/test/java/org/zongjieli/leetcode/weeklycompetition/WeeklyCompetitionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zongjieli.leetcode.weeklycompetition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeeklyCompetitionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
