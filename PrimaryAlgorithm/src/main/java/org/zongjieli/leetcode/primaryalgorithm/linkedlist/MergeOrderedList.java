6ef70925887d2e6a4c67f268904197fcafa753e3
LiZongjie <164260294@qq.com>
2020-01-13 14:31:29 +0800

    optimize

--- PrimaryAlgorithm/src/main/java/org/zongjieli/leetcode/primaryalgorithm/linkedlist/MergeOrderedList.java
+++ PrimaryAlgorithm/src/main/java/org/zongjieli/leetcode/primaryalgorithm/linkedlist/MergeOrderedList.java
@@ -9,34 +9,20 @@ package org.zongjieli.leetcode.primaryalgorithm.linkedlist;
  **/
 public class MergeOrderedList {
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        }
-        if (l2 == null) {
-            return l1;
-        }
-        if (l2.val < l1.val) {
-            ListNode moveNode = l2;
-            l2 = l2.next;
-            moveNode.next = l1;
-            l1 = moveNode;
-        }
-        ListNode head = l1;
-        while (l2 != null) {
-            if (l1.next == null) {
-                l1.next = l2;
-                return head;
-            }
-            while (l1.next != null && l1.next.val < l2.val) {
+        ListNode head = new ListNode(0);
+        ListNode moveNode = head;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val){
+                moveNode.next = l1;
                 l1 = l1.next;
+            } else {
+                moveNode.next = l2;
+                l2 = l2.next;
             }
-            ListNode moveNode = l1.next;
-            l1.next = l2;
-            l2 = l2.next;
-            l1.next.next = moveNode;
-            l1 = l1.next;
+            moveNode = moveNode.next;
         }
-        return head;
+        moveNode.next = l1 == null ? l2 : l1;
+        return head.next;
     }
 
     public static void main(String[] args) {
