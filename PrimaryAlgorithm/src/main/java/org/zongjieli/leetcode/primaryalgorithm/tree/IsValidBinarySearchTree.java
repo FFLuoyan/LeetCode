860b24d0a5d5d060f1181ed2cf5cbc584c6bfa01
LiZongjie <164260294@qq.com>
2020-01-17 09:39:45 +0800

    add lambda answer

--- PrimaryAlgorithm/src/main/java/org/zongjieli/leetcode/primaryalgorithm/tree/IsValidBinarySearchTree.java
+++ PrimaryAlgorithm/src/main/java/org/zongjieli/leetcode/primaryalgorithm/tree/IsValidBinarySearchTree.java
@@ -2,17 +2,34 @@ package org.zongjieli.leetcode.primaryalgorithm.tree;
 
 /**
  * @ClassName: IsValidBinarySearchTree
- * @Description: TODO
+ * @Description: Determine whether a binary tree is a binary search tree
  * @Author: Zongjie.Li
  * @Date: 2020/1/16
  * @Version: 1.0
  **/
 public class IsValidBinarySearchTree {
+
+    public interface NodeValidInter {
+        boolean nodeValid(TreeNode node);
+    }
+
+    public boolean isValidBstByLambda(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return nodeValidByLambda(root.left, L -> true, rootRight -> rootRight.val < root.val) ? nodeValidByLambda(root.right, rootLeft -> rootLeft.val > root.val, R -> true) : false;
+
+    }
+
     public boolean isValidBST(TreeNode root) {
         if (root == null) {
             return true;
         }
         return leftValid(root.left, Integer.MIN_VALUE, root.val) ? rightValid(root.right, root.val, Integer.MAX_VALUE) : false;
+    }
+
+    public boolean nodeValidByLambda(TreeNode node, NodeValidInter minValid, NodeValidInter maxValid) {
+        return node == null ? true : ((minValid.nodeValid(node) && maxValid.nodeValid(node)) ? (nodeValidByLambda(node.left, minValid, nodeRight -> nodeRight.val < node.val) ? nodeValidByLambda(node.right, nodeLeft -> nodeLeft.val > node.val, maxValid) : false) : false);
     }
 
     public boolean leftValid(TreeNode left, int minValue, int maxValue) {
@@ -37,7 +54,8 @@ public class IsValidBinarySearchTree {
 
     public static void main(String[] args) {
         IsValidBinarySearchTree tree = new IsValidBinarySearchTree();
-        System.out.println(tree.isValidBST(TreeNode.normalTreeNode()));
+        // System.out.println(tree.isValidBST(TreeNode.normalTreeNode()));
+        System.out.println(tree.isValidBST(TreeNode.binarySearchTree()));
     }
 
 
