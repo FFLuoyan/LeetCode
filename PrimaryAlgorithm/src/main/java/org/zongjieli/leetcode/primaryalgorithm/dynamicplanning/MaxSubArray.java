d85e47eb01f7ee3660ce4cfeb27d58592ee8c4b1
LiZongjie <164260294@qq.com>
2020-03-17 19:23:57 +0800

    新增计算数组中最大连续子数组和的算法类

---
+++ PrimaryAlgorithm/src/main/java/org/zongjieli/leetcode/primaryalgorithm/dynamicplanning/MaxSubArray.java
@@ -0,0 +1,22 @@
+package org.zongjieli.leetcode.primaryalgorithm.dynamicplanning;
+
+import java.util.LinkedList;
+
+/**
+ * @ClassName: MaxSubArray
+ * @Description: 给定一个数组, 求出该数组的最大大连子数组和
+ * @Author: Zongjie.Li
+ * @Date: 2020/3/16
+ * @Version: 1.0
+ **/
+public class MaxSubArray {
+    public static void main(String[] args) {
+        MaxSubArray maxSubArray = new MaxSubArray();
+        System.out.println(maxSubArray.maxSubArray(new int[]{1, 2, 4, 2, 0, 1, 3, -5, -5, -8, -6, -6, -1, 56, 3, 5, -85, 2, -5, -5, -5, 8, 5, -9, -6}));
+    }
+
+    public int maxSubArray(int[] nums) {
+        return 0;
+    }
+
+}
