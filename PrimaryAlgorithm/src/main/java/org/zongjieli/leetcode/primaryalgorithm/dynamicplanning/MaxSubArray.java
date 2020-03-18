6c7d521dcf57efb3e2e6a67219ca03d28807414c
LiZongjie <164260294@qq.com>
2020-03-18 10:28:51 +0800

    新增暴力解法

--- PrimaryAlgorithm/src/main/java/org/zongjieli/leetcode/primaryalgorithm/dynamicplanning/MaxSubArray.java
+++ PrimaryAlgorithm/src/main/java/org/zongjieli/leetcode/primaryalgorithm/dynamicplanning/MaxSubArray.java
@@ -1,7 +1,5 @@
 package org.zongjieli.leetcode.primaryalgorithm.dynamicplanning;
 
-import java.util.LinkedList;
-
 /**
  * @ClassName: MaxSubArray
  * @Description: 给定一个数组, 求出该数组的最大大连子数组和
@@ -10,13 +8,23 @@ import java.util.LinkedList;
  * @Version: 1.0
  **/
 public class MaxSubArray {
+
     public static void main(String[] args) {
         MaxSubArray maxSubArray = new MaxSubArray();
         System.out.println(maxSubArray.maxSubArray(new int[]{1, 2, 4, 2, 0, 1, 3, -5, -5, -8, -6, -6, -1, 56, 3, 5, -85, 2, -5, -5, -5, 8, 5, -9, -6}));
     }
 
     public int maxSubArray(int[] nums) {
-        return 0;
+        int maxValue = nums[0];
+        for (int index = 1 ; index < nums.length ; index++){
+            int countValue = 0;
+            for (int countIndex = index ; countIndex >=0 ;countIndex--){
+                countValue += nums[countIndex];
+                if (countValue > maxValue){
+                    maxValue = countValue;
+                }
+            }
+        }
+        return maxValue;
     }
-
 }
