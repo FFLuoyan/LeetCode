417a6e69b42e26d8a6efa5b4e13c605bb68a1c5a
LiZongjie <164260294@qq.com>
2020-03-27 13:36:47 +0800

    新增最大值解法

--- PrimaryAlgorithm/src/main/java/org/zongjieli/leetcode/primaryalgorithm/math/ThreePower.java
+++ PrimaryAlgorithm/src/main/java/org/zongjieli/leetcode/primaryalgorithm/math/ThreePower.java
@@ -10,42 +10,46 @@ import java.util.Arrays;
  * @Version: 1.0
  **/
 public class ThreePower {
-    public boolean isPowerOfThree (int n){
-        if (n < 1){
+    public boolean isPowerOfThree(int n) {
+        if (n < 1) {
             return false;
         }
-        while (n != 1){
-            if (n % 3 == 0){
+        while (n != 1) {
+            if (n % 3 == 0) {
                 n /= 3;
-            }else {
+            } else {
                 break;
             }
         }
-        if (n == 1){
+        if (n == 1) {
             return true;
         }
         return false;
     }
 
-    public boolean isPowerOfThreeByLog (int n){
+    public boolean isPowerOfThreeByLog(int n) {
         // 猜测,自然对数为底的对数函数,由于底较小,容易出现精度的问题,导致计算错误
         return ((Math.log10(n) % Math.log10(3)) % 1) == 0;
     }
 
-    public boolean isPowerOfThreeBinarySystem(int n){
-        if (n == 0){
+    public boolean isPowerOfThreeBinarySystem(int n) {
+        if (n == 0) {
             return false;
         }
         // 通过将数字转换相应的进制,计算该数是否为整倍数
-        String threeRadix = Integer.toString(n,3);
+        String threeRadix = Integer.toString(n, 3);
         // radix 基数
-        if (threeRadix.contains("2")){
+        if (threeRadix.contains("2")) {
             return false;
         }
-        if (threeRadix.indexOf("1",1) > -1){
+        if (threeRadix.indexOf("1", 1) > -1) {
             return false;
         }
         return true;
+    }
+
+    public boolean isPowerOfThreeByMaxValue(int n) {
+        return n <= 0 ? false : 1162261467 % n == 0;
     }
 
     public static void main(String[] args) {
