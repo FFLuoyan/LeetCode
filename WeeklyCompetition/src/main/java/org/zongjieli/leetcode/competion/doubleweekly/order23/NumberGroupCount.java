b0c6bd52fa33b2d6fcb3edfcece64a8bf1078afc
LiZongjie <164260294@qq.com>
2020-04-08 08:04:39 +0800

    假装 push(尚未完成)

--- WeeklyCompetition/src/main/java/org/zongjieli/leetcode/competion/doubleweekly/order23/NumberGroupCount.java
+++ WeeklyCompetition/src/main/java/org/zongjieli/leetcode/competion/doubleweekly/order23/NumberGroupCount.java
@@ -1,5 +1,7 @@
 package org.zongjieli.leetcode.competion.doubleweekly.order23;
 
+import java.util.Arrays;
+
 /**
  * @ClassName: NumberGroupCount
  * @Description: 给定整数n,求出 1 到 n 的每个整数 10 进制表示下的数位和
@@ -11,6 +13,30 @@ package org.zongjieli.leetcode.competion.doubleweekly.order23;
  **/
 public class NumberGroupCount {
     public int countLargestGroup(int n) {
+//        if (n < 10){
+//            return n;
+//        }
+//        int[] numberCount = new int[37];
+//
+//        if (n < 100){
+//            int row = (n / 10) + 1;
+//            // row 代表数字可以堆叠的高度
+//            // 如果 row > 10,则相同数位和的高度肯定有 2
+//            int column = (n % 10) + 1;
+//            // column 代表相同的个数,上一层相同的个数会得到继承
+//            // 如果 column 为 10,则该层遍历完
+//            int thisMax = 11 - row;
+//            // 本层的最大可能值
+//            return column < thisMax ? column : thisMax;
+//        }
+////        if (n < 1000){
+////            int first = n / 100 + 1;
+////
+////        }
+
+
+
+
         int[] numberMap = new int[37];
         for (int i = 1 ; i <= n;i++){
             int temp = i;
@@ -23,6 +49,7 @@ public class NumberGroupCount {
             int number = firstValue + secondValue + thirdValue + fourthValue;
             numberMap[number]++;
         }
+        System.out.println(Arrays.toString(numberMap));
         int max = 0;
         int size = 0;
         for (int number = 1;number < numberMap.length ; number++){
@@ -39,6 +66,6 @@ public class NumberGroupCount {
     public static void main(String[] args) {
 
         NumberGroupCount numberGroupCount = new NumberGroupCount();
-        System.out.println(numberGroupCount.countLargestGroup(13));
+        System.out.println(numberGroupCount.countLargestGroup(1000));
     }
 }
