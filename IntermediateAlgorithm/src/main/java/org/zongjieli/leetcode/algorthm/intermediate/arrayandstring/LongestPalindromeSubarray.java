f38197a34c0e8d940be89ea85e3ad6253dadf2f8
LiZongjie <164260294@qq.com>
2020-04-22 13:32:04 +0800

    计算字符串中的最长回文字符串

--- IntermediateAlgorithm/src/main/java/org/zongjieli/leetcode/algorthm/intermediate/arrayandstring/LongestPalindromeSubarray.java
+++ IntermediateAlgorithm/src/main/java/org/zongjieli/leetcode/algorthm/intermediate/arrayandstring/LongestPalindromeSubarray.java
@@ -19,21 +19,32 @@ public class LongestPalindromeSubarray {
         for (int charIndex = 1;charIndex < chars.length;charIndex++){
             // 字符串的每一个值开始循环
             // 循环的目的,去 can 数组中比较,如果为 true,说明为指定值时会产生回文字符串
-            palindromeCan[charIndex] = true;
             palindromeCan[charIndex - 1] = true;
+            if (charIndex > 1){
+                palindromeCan[charIndex - 2] = true;
+            }
             for (int canIndex = 0 ; canIndex < charIndex;canIndex++){
-                if (palindromeCan[canIndex + 1]){
-                    // 如果后一个位置可以组成回文字符串
+                if (palindromeCan[canIndex]){
+                    // 如果当前位置可以组成回文字符串
                     // 则对数组进行比较
                     if (chars[charIndex] == chars[canIndex]){
-                        // 如果比较字符与需要字符相等,说明当前位置在下一轮循环中也可以构成回文字符串
-                        palindromeCan[canIndex] = true;
+                        // 如果比较字符与需要字符相等,说明前一个位置在下一轮循环中可以构成回文字符串
+                        if (canIndex > 0){
+                            palindromeCan[canIndex - 1] = true;
+                        }
                         if (charIndex + 1 - canIndex > endIndex - startIndex){
                             startIndex = canIndex;
                             endIndex = charIndex + 1;
+                            System.out.println(s.substring(startIndex,endIndex));
                         }
                     } else {
-                        palindromeCan[canIndex] = false;
+                        if (canIndex > 0){
+                            palindromeCan[canIndex - 1] = false;
+                        }
+                    }
+                } else {
+                    if (canIndex > 0){
+                        palindromeCan[canIndex - 1] = false;
                     }
                 }
             }
@@ -43,7 +54,7 @@ public class LongestPalindromeSubarray {
 
     public static void main(String[] args) {
         LongestPalindromeSubarray subarray = new LongestPalindromeSubarray();
-        System.out.println(subarray.longestPalindrome("aaabaaaa"));
+        System.out.println(subarray.longestPalindrome("aaabaaaaaaaaaa"));
     }
 
 }
