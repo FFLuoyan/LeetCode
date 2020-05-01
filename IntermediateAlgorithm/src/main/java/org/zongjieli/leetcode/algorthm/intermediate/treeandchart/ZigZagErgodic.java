4e95fbb863d6ae97584e900dbbc73b1b76a081f4
LiZongjie <164260294@qq.com>
2020-05-01 22:58:44 +0800

    新增锯齿形遍历二叉树

---
+++ IntermediateAlgorithm/src/main/java/org/zongjieli/leetcode/algorthm/intermediate/treeandchart/ZigZagErgodic.java
@@ -0,0 +1,11 @@
+package org.zongjieli.leetcode.algorthm.intermediate.treeandchart;
+
+/**
+ * @ClassName: ZigZagErgodic
+ * @Description: 返回给定二叉树的锯齿状层次遍历
+ * @Author: Zongjie.Li
+ * @Date: 2020/5/1
+ * @Version: 1.0
+ **/
+public class ZigZagErgodic {
+}
