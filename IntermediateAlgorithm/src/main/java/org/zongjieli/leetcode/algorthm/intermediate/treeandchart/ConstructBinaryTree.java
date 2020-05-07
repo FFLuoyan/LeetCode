0eb80296f4bf8fb58774f394b41944d77000f54a
LiZongjie <164260294@qq.com>
2020-05-07 08:22:53 +0800

    新增根据前序遍历及中序遍历构建二叉树的递归暴力方法

--- IntermediateAlgorithm/src/main/java/org/zongjieli/leetcode/algorthm/intermediate/treeandchart/ConstructBinaryTree.java
+++ IntermediateAlgorithm/src/main/java/org/zongjieli/leetcode/algorthm/intermediate/treeandchart/ConstructBinaryTree.java
@@ -1,5 +1,7 @@
 package org.zongjieli.leetcode.algorthm.intermediate.treeandchart;
 
+import org.zongjieli.leetcode.algorithm.primary.tree.TreeNode;
+
 /**
  * @ClassName: ConstructBinaryTree
  * @Description: 根据中序遍历以及前序遍历构建二叉树(没有重复元素)
@@ -8,4 +10,34 @@ package org.zongjieli.leetcode.algorthm.intermediate.treeandchart;
  * @Version: 1.0
  **/
 public class ConstructBinaryTree {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || inorder == null || preorder.length ==0 || inorder.length == 0){
+            return null;
+        }
+        return buildTree(inorder,preorder,0,preorder.length - 1,0,inorder.length - 1);
+    }
+
+
+    public TreeNode buildTree(int[] preorder, int[] inorder,int preStart,int preEnd,int inStart,int inEnd){
+        if (preStart > preEnd){
+            return null;
+        }
+        TreeNode root = new TreeNode(inorder[inStart]);
+        int middleNodeIndex = preStart;
+        while (preorder[middleNodeIndex] != inorder[inStart]){
+            // 如果前序遍历节点值不等于中序遍历的中点值,说明子树位置没有改变
+            middleNodeIndex++;
+        }
+        // 此时 inorder[inStart] == preOrder[middleNodeIndex]
+        // middleNodeIndex 节点的位置即为前序遍历中中点的位置
+        root.left = buildTree(preorder,inorder,preStart,middleNodeIndex - 1,inStart + 1,inStart + middleNodeIndex - preStart);
+        root.right = buildTree(preorder,inorder,middleNodeIndex + 1,preEnd,inEnd - preEnd + middleNodeIndex + 1,inEnd);
+        return root;
+    }
+
+    public static void main(String[] args) {
+        ConstructBinaryTree constructBinaryTree = new ConstructBinaryTree();
+        TreeNode root = constructBinaryTree.buildTree(new int[]{3,9,20,15,7},new int[]{9,3,15,20,7});
+        System.out.println(root.val);
+    }
 }
