bf5b3eeb68ca3d673fbf4fd77e0bf05769b61c5e
LiZongjie <164260294@qq.com>
2020-05-15 22:16:35 +0800

    新增岛屿计算的算法

---
+++ IntermediateAlgorithm/src/main/java/org/zongjieli/leetcode/algorthm/intermediate/treeandchart/IslandCount.java
@@ -0,0 +1,17 @@
+package org.zongjieli.leetcode.algorthm.intermediate.treeandchart;
+
+/**
+ * @ClassName: IslandCount
+ * @Description: 根据给定的二维数组,计算出数组中岛屿的数量
+ *               '1' 表示陆地,'0' 表示海水
+ *               岛屿总是被水包围着,并且每座岛屿只能由水平方向竖直方向上相邻的陆地连接形成
+ *               可以假想该网格的四周均被水包裹
+ * @Author: Zongjie.Li
+ * @Date: 2020/5/15
+ * @Version: 1.0
+ **/
+public class IslandCount {
+    public int numIslands(char[][] grid) {
+        return 0;
+    }
+}
