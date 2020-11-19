42588f89e7f456d442f7bcd56d812c20faf00da6
LiZongjie <164260294@qq.com>
2020-11-19 22:51:35 +0800

    新增股票交易算法的动态规划解法,并增加解题思路注解

--- PrimaryAlgorithm/src/main/java/org/zongjieli/leetcode/algorithm/primary/array/BestTimeToTransactionShares.java
+++ PrimaryAlgorithm/src/main/java/org/zongjieli/leetcode/algorithm/primary/array/BestTimeToTransactionShares.java
@@ -10,6 +10,13 @@ package org.zongjieli.leetcode.algorithm.primary.array;
  * @Version: 1.0
  **/
 public class BestTimeToTransactionShares {
+
+    /**
+     * 该算法通过贪心算法来实现,贪心算法是动态规划法的一个特例
+     * 贪心算法要求每一步均为最优解
+     * 而动态规划则要求上一步可能为局部最优解,也可能不为局部最优解
+     *
+     */
     public int maxProfit(int[] prices) {
         int profits = 0;
         for (int i = 1; i < prices.length; i++){
@@ -18,8 +25,31 @@ public class BestTimeToTransactionShares {
         return profits;
     }
 
+    /**
+     * Dp(Dynamic programming)
+     * 该题动态规划的解决方案的要点在于找到合适的状态转移方程
+     * 即每日可获得的最高收益与前一日的收益关系
+     *
+     * 每日的状态为 当前收益、是否持有股票
+     * profitNoShares 可以保证每天的收益都是最大的
+     * profitWitShares 可以保证每天手上的股票都是最便宜的
+     */
+    public int maxProfitByDp(int[] prices) {
+        int profitNoShares = 0;
+        int profitWitShares = - prices[0];
+
+        for (int i = 1 ; i < prices.length ; i++){
+            int temp = profitNoShares;
+            profitNoShares = Math.max(profitWitShares + prices[i],temp);
+            profitWitShares = Math.max(temp - prices[i],profitWitShares);
+        }
+
+        return profitNoShares;
+    }
+
     public static void main(String[] args) {
         BestTimeToTransactionShares shares = new BestTimeToTransactionShares();
         System.out.println(shares.maxProfit(new int[]{1, 5, 1, 0, 3, 2, 6, 5, 7, 8, 5, 4, 1, 0, 2}));
+        System.out.println(shares.maxProfitByDp(new int[]{1, 5, 1, 0, 3, 2, 6, 5, 7, 8, 5, 4, 1, 0, 2}));
     }
 }
