f583a9ec0e1e083cf00bf9e1fc8d6b8872d10bcc
LiZongjie <164260294@qq.com>
2021-01-25 14:40:20 +0800

    优化代码、新增双数组解决方式

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month1/ArrayAdd.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month1/ArrayAdd.java
@@ -1,5 +1,6 @@
 package org.zongjieli.leetcode.question.daily.year2021.month1;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -19,25 +20,38 @@ import java.util.List;
 public class ArrayAdd {
     public List<Integer> addToArrayForm(int[] A, int K) {
         LinkedList<Integer> result = new LinkedList<>();
-        for (int index = A.length - 1 ; index >= 0 ; index --){
-            int sum = K % 10 + A[index];
-            K /= 10;
-            if (sum >= 10){
-                sum -= 10;
-                K ++;
+        for (int index = A.length - 1 ; index >= 0 || K > 0;){
+            if (index >= 0){
+                K += A[index--];
             }
-            result.addFirst(sum);
-        }
-        while (K > 0){
-            result.addFirst(K%10);
+            result.addFirst(K % 10);
             K /= 10;
+        }
+        return result;
+    }
+
+    public List<Integer> addToArrayFormByArray(int[] A, int K) {
+        int[] answer = new int[A.length > 5 ? A.length + 1 : 6];
+        int valueIndex = answer.length;
+        int addIndex = A.length - 1;
+        while (addIndex >= 0 || K > 0){
+            if (addIndex >= 0){
+                K += A[addIndex --];
+            }
+            answer[--valueIndex] = K % 10;
+            K /= 10;
+        }
+        List<Integer> result = new ArrayList<>(answer.length);
+        for (int i = valueIndex ; i < answer.length ; i++){
+            result.add(answer[i]);
         }
         return result;
     }
 
     public static void main(String[] args) {
         ArrayAdd arrayAdd = new ArrayAdd();
-        System.out.println(arrayAdd.addToArrayForm(new int[]{7,1,5,9,9,9,6,4, 8,7,},425613220));
+        System.out.println(arrayAdd.addToArrayForm(new int[]{8,7},782));
+        System.out.println(arrayAdd.addToArrayFormByArray(new int[]{ 8,7},782));
     }
 
 }
