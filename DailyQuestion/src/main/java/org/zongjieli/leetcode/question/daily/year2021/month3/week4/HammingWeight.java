05897db0478042d326fbcba5bfa38649f33ea468
LiZongjie <164260294@qq.com>
2021-03-22 09:48:18 +0800

    ADD 新增汉明重量的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month3/week4/HammingWeight.java
@@ -0,0 +1,25 @@
+package org.zongjieli.leetcode.question.daily.year2021.month3.week4;
+/**
+ * 编写一个函数,输入一个无符号整数(二进制串)
+ * 返回其二进制表达式中数字位数为 '1' 的个数(汉明重量)
+ *
+ * @author   Zongjie.Li
+ * @date     2021/3/22
+ * @version  1.0
+ */
+public class HammingWeight {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0){
+            count += (n & 1);
+            n >>>= 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        HammingWeight hammingWeight = new HammingWeight();
+        System.out.println( -1 >>> 1);
+//        System.out.println(hammingWeight.hammingWeight(-1));
+    }
+}
