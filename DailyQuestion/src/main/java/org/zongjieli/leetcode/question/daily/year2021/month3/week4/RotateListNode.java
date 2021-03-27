169211ab4c070f4251aaa1b9fdefb5700ed56214
LiZongjie <164260294@qq.com>
2021-03-27 23:57:17 +0800

    ADD 完成旋转链表的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month3/week4/RotateListNode.java
@@ -0,0 +1,39 @@
+package org.zongjieli.leetcode.question.daily.year2021.month3.week4;
+
+import org.zongjieli.leetcode.base.ListNode;
+
+/**
+ * 给你一个链表的头结点,旋转链表
+ * 将链表每个节点向右移动 k 个位置
+ *
+ * @author   Zongjie.Li
+ * @date     2021/3/27
+ * @version  1.0
+ */
+public class RotateListNode {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null){
+            return null;
+        }
+        int size = 1;
+        ListNode loop = head;
+        while (loop.next != null){
+            size++;
+            loop = loop.next;
+        }
+        int right = k % size;
+        if (right == 0){
+            return head;
+        }
+        loop.next = head;
+        int nextNull = size - right;
+        loop = head;
+        while (nextNull > 1){
+            nextNull--;
+            loop = loop.next;
+        }
+        head = loop.next;
+        loop.next = null;
+        return head;
+    }
+}
