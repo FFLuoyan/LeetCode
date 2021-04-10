6bbf9a75a8db1e0fe0606c5afaf573e0388b59ad
LiZongjie <164260294@qq.com>
2021-04-10 09:48:33 +0800

    ADD 完成丑数判断的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month4/week2/UglyNumber.java
@@ -0,0 +1,28 @@
+package org.zongjieli.leetcode.question.daily.year2021.month4.week2;
+/**
+ * 给你一个整数 n,请你判断 n 是否为丑数
+ * 如果是,返回 true,否则返回 false
+ *
+ * 丑数: 只包含质因数 2、3 或 5 的正整数
+ *
+ * @author   Zongjie.Li
+ * @date     2021/4/10
+ * @version  1.0
+ */
+public class UglyNumber {
+    public boolean isUgly(int n) {
+        if (n == 0){
+            return false;
+        }
+        while (n % 2 == 0){
+            n = n / 2;
+        }
+        while (n % 3 == 0){
+            n = n / 3;
+        }
+        while (n % 5 == 0){
+            n = n / 5;
+        }
+        return n == 1;
+    }
+}
