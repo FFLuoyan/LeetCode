864a2b0e5a80397fd38a329288eab1918ac767d4
LiZongjie <164260294@qq.com>
2021-04-13 14:54:21 +0800

    ADD 完成二叉搜索树中最小节点差值计算的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month4/week3/MinDistanceInBinarySearchTree.java
@@ -0,0 +1,39 @@
+package org.zongjieli.leetcode.question.daily.year2021.month4.week3;
+
+import org.zongjieli.leetcode.base.TreeNode;
+
+import java.util.LinkedList;
+
+/**
+ * 给定一个二叉搜索树的根节点 root
+ * 返回树中任意两不同节点值之间的最小差值
+ *
+ * @author   Zongjie.Li
+ * @date     2021/4/13
+ * @version  1.0
+ */
+public class MinDistanceInBinarySearchTree {
+
+    public int minDiffInBST(TreeNode root) {
+        LinkedList<TreeNode> list = new LinkedList<>();
+        list.addFirst(root);
+        root = root.left;
+        while (root != null){
+            list.addFirst(root);
+            root = root.left;
+        }
+        long min = Integer.MAX_VALUE;
+        long temp = Integer.MIN_VALUE;
+        while (!list.isEmpty()){
+            TreeNode minNode = list.poll();
+            min = Math.min(min,minNode.val - temp);
+            temp = minNode.val;
+            minNode = minNode.right;
+            while (minNode != null){
+                list.addFirst(minNode);
+                minNode = minNode.left;
+            }
+        }
+        return (int) min;
+    }
+}
