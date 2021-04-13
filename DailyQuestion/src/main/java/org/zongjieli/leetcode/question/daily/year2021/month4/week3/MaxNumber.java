413f81a19a90c8762ddb2e1f58d9492541b01118
LiZongjie <164260294@qq.com>
2021-04-13 11:11:14 +0800

    IMP 优化原有版本,两个方法合并

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month4/week3/MaxNumber.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month4/week3/MaxNumber.java
@@ -54,6 +54,69 @@ public class MaxNumber {
         return first * secondBit + second - second * firstBit - first > 0;
     }
 
+    public String largestNumberFast(int[] nums) {
+        int[][] compare = new int[nums.length][];
+        int compareIndex = 0;
+        while (compareIndex < nums.length){
+            compare[compareIndex] = numToArray(nums[compareIndex]);
+
+            int compareArrayIndex = compareIndex - 1;
+            while (compareArrayIndex >= 0 && isBiggerFast(compare[compareArrayIndex + 1],compare[compareArrayIndex])){
+                int[] temp = compare[compareArrayIndex + 1];
+                int numTemp = nums[compareArrayIndex + 1];
+                nums[compareArrayIndex + 1] = nums[compareArrayIndex];
+                compare[compareArrayIndex + 1] = compare[compareArrayIndex];
+                nums[compareArrayIndex] = numTemp;
+                compare[compareArrayIndex] = temp;
+                compareArrayIndex --;
+            }
+
+            compareIndex ++;
+        }
+
+        if (nums[0] == 0){
+            return "0";
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int num : nums) {
+            stringBuilder.append(num);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    public int[] numToArray(int num){
+        if (num == 0){
+            return new int[]{0};
+        }
+        int bit = num;
+        int bitCount = 0;
+        while (bit > 0){
+            bit = bit / 10;
+            bitCount ++;
+        }
+        int[] numArray = new int[bitCount];
+        while (num > 0){
+            numArray[--bitCount] = num % 10;
+            num /= 10;
+        }
+        return numArray;
+    }
+
+    private boolean isBiggerFast(int[] first, int[] second){
+        int index = -1;
+        while (++index < first.length + second.length){
+            int firstCompare = index < first.length ? first[index] : second[index - first.length];
+            int secondCompare = index < second.length ? second[index] : first[index - second.length];
+            if (firstCompare == secondCompare){
+                continue;
+            }
+            return firstCompare > secondCompare;
+        }
+        return false;
+    }
+
+
     public static void main(String[] args) {
         MaxNumber number = new MaxNumber();
         System.out.println(number.largestNumber(new int[]{1,54651,30,321,54}));;
