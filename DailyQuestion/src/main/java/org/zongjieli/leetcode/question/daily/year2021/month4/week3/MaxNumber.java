87f3f623bba2b46496b64ef9190628aafce31db2
LiZongjie <164260294@qq.com>
2021-04-13 10:57:14 +0800

    IMP 简化最大数的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month4/week3/MaxNumber.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month4/week3/MaxNumber.java
@@ -1,7 +1,5 @@
 package org.zongjieli.leetcode.question.daily.year2021.month4.week3;
 
-import java.util.Arrays;
-
 /**
  * 给定一组非负整数 nums
  * 重新排列每个数的顺序,每个数不可拆分
@@ -15,74 +13,49 @@ import java.util.Arrays;
  */
 public class MaxNumber {
     public String largestNumber(int[] nums) {
-        int[][] compare = new int[nums.length][];
-        int compareIndex = 0;
-        while (compareIndex < nums.length){
-            compare[compareIndex] = numToArray(nums[compareIndex]);
 
-            int compareArrayIndex = compareIndex - 1;
-            while (compareArrayIndex >= 0 && isBigger(compare[compareArrayIndex + 1],compare[compareArrayIndex])){
-                int[] temp = compare[compareArrayIndex + 1];
-                compare[compareArrayIndex + 1] = compare[compareArrayIndex];
-                compare[compareArrayIndex] = temp;
-                compareArrayIndex --;
+        int index = 1;
+        while (index < nums.length){
+            int compareIndex = index - 1;
+            while (compareIndex >= 0 && isBigger(nums[compareIndex + 1],nums[compareIndex])){
+                int temp = nums[compareIndex];
+                nums[compareIndex] = nums[compareIndex + 1];
+                nums[compareIndex + 1] = temp;
+                compareIndex --;
             }
-
-            compareIndex ++;
+            index ++;
         }
 
-        if (compare[0][0] == 0){
+        if (nums[0] == 0){
             return "0";
         }
         StringBuilder stringBuilder = new StringBuilder();
-        for (int[] ints : compare) {
-            for (int anInt : ints) {
-                stringBuilder.append(anInt);
-            }
+        for (int num : nums) {
+            stringBuilder.append(num);
         }
 
         return stringBuilder.toString();
     }
 
-    public int[] numToArray(int num){
-        if (num == 0){
-            return new int[]{0};
+    private boolean isBigger(int first, int second){
+        if (first == 0){
+            return false;
         }
-        int bit = num;
-        int bitCount = 0;
-        while (bit > 0){
-            bit = bit / 10;
-            bitCount ++;
+        if (second == 0){
+            return true;
         }
-        int[] numArray = new int[bitCount];
-        while (num > 0){
-            numArray[--bitCount] = num % 10;
-            num /= 10;
+        long firstBit = 10,secondBit = 10,firstCompare = first,secondCompare = second;
+        while ((firstCompare /= 10) > 0){
+            firstBit *= 10;
         }
-        return numArray;
-    }
-
-    private boolean isBigger(int[] first, int[] second){
-        int firstIndex = 0;
-        int secondIndex = 0;
-        while (first[firstIndex] == second[secondIndex]){
-            firstIndex++;
-            secondIndex++;
-            if (firstIndex == first.length){
-                if (secondIndex == second.length){
-                    return false;
-                }
-                firstIndex -= first.length;
-                continue;
-            }
-            secondIndex %= second.length;
+        while ((secondCompare /= 10) > 0){
+            secondBit *= 10;
         }
-        return first[firstIndex] > second[secondIndex];
+        return first * secondBit + second - second * firstBit - first > 0;
     }
 
     public static void main(String[] args) {
         MaxNumber number = new MaxNumber();
-        System.out.println(Arrays.toString(number.numToArray(10)));
         System.out.println(number.largestNumber(new int[]{1,54651,30,321,54}));;
     }
 }
