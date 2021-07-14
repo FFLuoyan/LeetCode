d5cc144061f7c9b1574c5ec077bb28ad340637d2
LiZongjie <164260294@qq.com>
2021-07-14 13:36:46 +0800

    IMP 优化求绝对差值和的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month7/week3/SumAbsoluteValue.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month7/week3/SumAbsoluteValue.java
@@ -27,36 +27,28 @@ import java.util.*;
  */
 public class SumAbsoluteValue {
     public int minAbsoluteSumDiff(int[] nums1, int[] nums2) {
-        TreeMap<Integer, List<Integer>> absMap = new TreeMap<>();
         TreeSet<Integer> nums1Set = new TreeSet<>();
-        for (int i = 0; i < nums1.length; i++) {
-            int abs = Math.abs(nums1[i] - nums2[i]);
-            List<Integer> indexes = absMap.computeIfAbsent(abs, k -> new ArrayList<>());
-            indexes.add(i);
-            nums1Set.add(nums1[i]);
+        for (int value : nums1) {
+            nums1Set.add(value);
         }
         int max = 0;
-        Integer abs = absMap.lastKey();
-        while (abs != null && abs > max){
-            List<Integer> nums1Indexes = absMap.get(abs);
-            for (int index : nums1Indexes) {
-                int start = nums2[index] > nums1[index] ? nums1[index] : nums2[index] - abs;
-                int end = start + 2 * abs;
+        long result = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            int abs = Math.abs(nums2[i] - nums1[i]);
+            result += abs;
+            if (abs > max){
+                int maxDiff = abs - max;
+                int start = nums2[i] - maxDiff;
+                int end = nums2[i] + maxDiff;
                 Integer loopKey = nums1Set.ceiling(start);
                 while (loopKey != null && loopKey < end) {
-                    int updateAbs = Math.abs(nums2[index] - loopKey);
+                    int updateAbs = Math.abs(nums2[i] - loopKey);
                     max = Math.max(max, abs - updateAbs);
                     loopKey = nums1Set.higher(loopKey);
                 }
-                if (max >= abs){
-                    break;
-                }
             }
-            abs = absMap.lowerKey(abs);
         }
-        long[] result = new long[]{0};
-        absMap.forEach((k,v) -> result[0] += k * v.size());
-        return (int) ((result[0] - max) % 1000000007);
+        return (int) ((result - max) % 1000000007);
     }
 
     public static void main(String[] args) {
@@ -65,13 +57,8 @@ public class SumAbsoluteValue {
 //        int[] nums2 = new int[]{2,3,4};
 //        int[] nums1 = new int[]{1,7,5};
 //        int[] nums2 = new int[]{2,3,5};
-        int[] nums1 = new int[100000];
-        int[] nums2 = new int[100000];
-        Random random = new Random();
-        for (int i = 0; i < nums1.length; i++) {
-            nums1[i] = random.nextInt(100000);
-            nums2[i] = random.nextInt(100000);
-        }
+        int[] nums1 = new int[]{57,42,21,28,30,25,22,12,55,3,47,18,43,29,20,44,59,9,43,7,8,5,42,53,99,34,37,88,87,62,38,68,31,3,11,61,93,34,63,27,20,48,38,5,71,100,88,54,52,15,98,59,74,26,81,38,11,44,25,69,79,81,51,85,59,84,83,99,31,47,31,23,83,70,82,79,86,31,50,17,11,100,55,15,98,11,90,16,46,89,34,33,57,53,82,34,25,70,5,1};
+        int[] nums2 = new int[]{76,3,5,29,18,53,55,79,30,33,87,3,56,93,40,80,9,91,71,38,35,78,32,58,77,41,63,5,21,67,21,84,52,80,65,38,62,99,80,13,59,94,21,61,43,82,29,97,31,24,95,52,90,92,37,26,65,89,90,32,27,3,42,47,93,25,14,5,39,85,89,7,74,38,12,46,40,25,51,2,19,8,21,62,58,29,32,77,62,9,74,98,10,55,25,62,48,48,24,21};
         System.out.println(test.minAbsoluteSumDiff(nums1,nums2));
     }
 }
