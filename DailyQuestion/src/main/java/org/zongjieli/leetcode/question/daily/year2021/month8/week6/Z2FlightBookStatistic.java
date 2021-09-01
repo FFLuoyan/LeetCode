c606bb2607c815a083d71d145ed9be7746a846fb
LiZongjie <164260294@qq.com>
2021-09-01 17:20:09 +0800

    IMP 优化统计预定航班数量的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month8/week6/Z2FlightBookStatistic.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month8/week6/Z2FlightBookStatistic.java
@@ -20,9 +20,13 @@ public class Z2FlightBookStatistic {
     public int[] corpFlightBookings(int[][] bookings, int n) {
         int[] answer = new int[n];
         for (int[] booking : bookings) {
-            for (int i = booking[0] ; i <= booking[1] ; i++){
-                answer[i - 1] += booking[2];
+            answer[booking[0] - 1] += booking[2];
+            if (booking[1] < n){
+                answer[booking[1]] -= booking[2];
             }
+        }
+        for (int i = 1 ; i < n ; i++){
+            answer[i] += answer[i - 1];
         }
         return answer;
     }
