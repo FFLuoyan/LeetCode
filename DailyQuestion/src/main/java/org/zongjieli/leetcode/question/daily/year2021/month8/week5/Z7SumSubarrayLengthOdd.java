d3d62f9b7f197618df0cac4a756b37baf11215d3
LiZongjie <164260294@qq.com>
2021-09-18 11:28:02 +0800

    IMP 优化计算所有奇数长度子数组的和的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month8/week5/Z7SumSubarrayLengthOdd.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month8/week5/Z7SumSubarrayLengthOdd.java
@@ -15,15 +15,20 @@ package org.zongjieli.leetcode.question.daily.year2021.month8.week5;
  */
 public class Z7SumSubarrayLengthOdd {
     public int sumOddLengthSubarray(int[] arr) {
-        int result = 0;
-        for (int i = 0 ; i < arr.length ; i++){
-            int temp = i;
-            int add = arr[temp];
-            result += add;
-            while (temp > 1){
-                add += (arr[--temp] + arr[--temp]);
-                result += add;
+        int result = arr[0];
+        int add = arr[0];
+        // 奇数位置和
+        int oddSum = arr[0];
+        int arrayCount = 1;
+        for (int i = 1 ; i < arr.length ; i++){
+            if ((i & 1) == 1){
+                // 奇数,奇数下标为偶数位置
+                add += (arrayCount * arr[i] - oddSum);
+            } else {
+                // 偶数,偶数下标为奇数位置
+                add += (arrayCount++ * arr[i] + (oddSum += arr[i]));
             }
+            result += add;
         }
         return result;
     }
