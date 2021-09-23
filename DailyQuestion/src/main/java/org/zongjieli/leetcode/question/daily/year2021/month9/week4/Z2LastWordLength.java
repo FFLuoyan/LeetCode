2a0aa32c6c4e0411bd6976131d758462a0eac54c
LiZongjie <164260294@qq.com>
2021-09-23 16:23:09 +0800

    ADD 完成计算字符串中最后一个单词长度的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month9/week4/Z2LastWordLength.java
@@ -0,0 +1,25 @@
+package org.zongjieli.leetcode.question.daily.year2021.month9.week4;
+/**
+ * 给定一个字符串 s,由若干单词组成,单词前后用一些空格字符隔开
+ * 返回字符串中最后一个单词的长度
+ * 单词是指仅由字母组成、不包含任何空格字符的最大子字符串
+ *
+ * 1 <= s.length <= 10^4
+ * s 仅有英文字母和空格 ' ' 组成
+ * s 中至少存在一个单词
+ *
+ * @author   Li.zongjie
+ * @date     2021/9/23
+ * @version  1.0
+ */
+public class Z2LastWordLength {
+    public int lengthOfLastWord(String s) {
+        String[] split = s.split(" ");
+        return split[split.length - 1].length();
+    }
+
+    public static void main(String[] args) {
+        Z2LastWordLength test = new Z2LastWordLength();
+        System.out.println(test.lengthOfLastWord("asd"));
+    }
+}
