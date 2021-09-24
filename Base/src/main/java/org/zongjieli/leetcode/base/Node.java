23941791880a4d68965739fc9e7d6b5d65c7d0de
LiZongjie <164260294@qq.com>
2021-09-24 10:21:22 +0800

    ADD 新增多级双向链表类

---
+++ Base/src/main/java/org/zongjieli/leetcode/base/Node.java
@@ -0,0 +1,16 @@
+package org.zongjieli.leetcode.base;
+/**
+ * 多级双向链表
+ *
+ * @author   Li.zongjie
+ * @date     2021/9/24
+ * @version  1.0
+ */
+public class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+
+
+}
