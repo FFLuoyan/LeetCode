5fcfd1bf0ed182b7c7741c603d1b911e38430278
LiZongjie <164260294@qq.com>
2021-10-09 09:55:58 +0800

    IMP 优化查找字符串中出现至少两次的子串的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month10/week2/Z5RepeatedDna.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month10/week2/Z5RepeatedDna.java
@@ -1,9 +1,7 @@
 package org.zongjieli.leetcode.question.daily.year2021.month10.week2;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * 所有 DNA 都由一系列缩写为 'A','C','G' 和 'T' 的核苷酸组成
@@ -21,25 +19,43 @@ import java.util.Map;
  */
 public class Z5RepeatedDna {
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> result = new ArrayList<>();
+        List<String> result = new ArrayList<>(2 * s.length());
         if (s.length() <= 10){
             return result;
         }
-        Map<String,Boolean> save = new HashMap<>(2 * s.length());
-        for (int i = 10 ; i <= s.length() ; i++){
-            String value = s.substring(i - 10, i);
-            save.merge(value,false, (old,v) -> true);
+        short[] count = new short[1048576];
+        int value = 0;
+        for (int i = 0 ; i < 10 ; i++){
+            char c = s.charAt(i);
+            int cI = c == 'A' ? 0 : (c == 'C' ? 1 : (c == 'G' ? 2 : 3));
+            value = value << 2;
+            value += cI;
         }
-        save.forEach((k,v) -> {
-            if (v){
-                result.add(k);
+        count[value]++;
+
+        char[] temp = new char[10];
+        for (int i = 10 ; i < s.length() ; i++){
+            char c = s.charAt(i);
+            int cI = c == 'A' ? 0 : (c == 'C' ? 1 : (c == 'G' ? 2 : 3));
+            value = (((value << 2) + cI) & 1048575);
+            count[value]++;
+            if (count[value] == 2){
+                int a = value;
+                for (int j = 9 ; j >= 0 ; j--){
+                    int last = a & 3;
+                    temp[j] = last == 0 ? 'A' : (last == 1 ? 'C' : (last == 2 ? 'G' : 'T'));
+                    a = a >> 2;
+                }
+                result.add(new String(temp));
             }
-        });
+        }
+
         return result;
     }
 
     public static void main(String[] args) {
         Z5RepeatedDna test = new Z5RepeatedDna();
-        System.out.println(test.findRepeatedDnaSequences("AAAAAAAAAAA"));
+//        System.out.println(test.findRepeatedDnaSequences("AAAAAAAAAAA"));
+        System.out.println(test.findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"));
     }
 }
