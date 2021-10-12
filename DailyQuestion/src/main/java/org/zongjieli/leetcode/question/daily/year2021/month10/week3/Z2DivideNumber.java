b6216cb5b571f35af11b69691c32e18593efd06e
LiZongjie <164260294@qq.com>
2021-10-12 14:33:02 +0800

    ADD 完成不使用 *,/,% 完成两个整数的除法计算的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month10/week3/Z2DivideNumber.java
@@ -0,0 +1,90 @@
+package org.zongjieli.leetcode.question.daily.year2021.month10.week3;
+/**
+ * 给定两个整数,被除数 dividend 和除数 divisor
+ * 将两数相除,要求不使用乘法、除法和 mod 运算符
+ *
+ * 返回被除数 dividend 除以除数 divisor 得到的商
+ * 整数除法的结果应当截去(truncate)其小数部分
+ * 例如: truncate(8.345) = 8 以及 truncate(-2.7335) = -2
+ *
+ * 被除数和除数均为 32 位有符号整数
+ * 除数不为 0
+ * 假设我们的环境只能存储 32 位有符号整数
+ * 其数值范围是 [−2^31,  2^31 − 1]
+ * 本题中,如果除法结果溢出,则返回 2^31 − 1
+ *
+ *
+ * @author   Li.zongjie
+ * @date     2021/10/12
+ * @version  1.0
+ */
+public class Z2DivideNumber {
+    public int divide(int dividend, int divisor) {
+        if (dividend == 0){
+            return 0;
+        }
+        if (dividend > 0){
+            if (divisor > dividend || divisor < -dividend){
+                return 0;
+            }
+            int signal = divisor < 0 ? -1 : 1;
+            divisor = divisor * signal;
+            int multiple = 1;
+            while (divisor < dividend && divisor <= Integer.MAX_VALUE / 2){
+                multiple <<= 1;
+                divisor <<= 1;
+            }
+
+            int result = 0;
+            while (multiple >= 1){
+                if (dividend >= divisor){
+                    dividend -= divisor;
+                    result += multiple;
+                }
+                divisor >>= 1;
+                multiple >>= 1;
+            }
+            return signal * result;
+        }
+
+        if (dividend == Integer.MIN_VALUE){
+            if (divisor == -1){
+                return Integer.MAX_VALUE;
+            }
+            if (divisor == 1){
+                return Integer.MIN_VALUE;
+            }
+        }
+        if (divisor < dividend || -divisor < dividend){
+            return 0;
+        }
+        int signal = divisor < 0 ? 1 : -1;
+        divisor *= signal;
+        int multiple = 1;
+        while (divisor > dividend && divisor >= Integer.MIN_VALUE / 2){
+            multiple <<= 1;
+            divisor <<= 1;
+        }
+
+        int result = 0;
+        while (multiple >= 1){
+            if (dividend <= divisor){
+                dividend -= divisor;
+                result += multiple;
+            }
+            divisor >>= 1;
+            multiple >>= 1;
+        }
+        return signal * result;
+    }
+
+    public static void main(String[] args) {
+        Z2DivideNumber test = new Z2DivideNumber();
+//        System.out.println(test.divide(10,3));
+//        System.out.println(test.divide(-10,3));
+//        System.out.println(test.divide(10,-3));
+//        System.out.println(test.divide(-10,-3));
+//        System.out.println(test.divide(1,1));
+        System.out.println(test.divide(Integer.MIN_VALUE,2));
+    }
+}
