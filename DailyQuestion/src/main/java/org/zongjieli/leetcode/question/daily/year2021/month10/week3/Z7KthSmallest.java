12e0f4089517c301266f1c4f280ed0300e794da4
LiZongjie <164260294@qq.com>
2021-10-18 10:51:37 +0800

    ADD 完成查找二叉搜索树中第 k 小的节点的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month10/week3/Z7KthSmallest.java
@@ -0,0 +1,42 @@
+package org.zongjieli.leetcode.question.daily.year2021.month10.week3;
+
+import org.zongjieli.leetcode.base.TreeNode;
+
+import java.util.LinkedList;
+
+/**
+ * 给定一个二叉搜索树的根节点 root 和一个整数 k
+ * 请设计一个算法,查找其中第 k 个最小元素(从 1 开始计数)
+ *
+ * 树中的节点数为 n
+ * 1 <= k <= n <= 10^4
+ * 0 <= Node.val <= 10^4
+ *
+ * @author   Li.zongjie
+ * @date     2021/10/18
+ * @version  1.0
+ */
+public class Z7KthSmallest {
+    public int kthSmallest(TreeNode root, int k) {
+        LinkedList<TreeNode> pool = new LinkedList<>();
+        while (root != null){
+            pool.addLast(root);
+            root = root.left;
+        }
+        int count = 0;
+        while (pool.size() != 0){
+            TreeNode current = pool.pollLast();
+            count++;
+            if (count == k){
+                return current.val;
+            }
+            current = current.right;
+            while (current != null){
+                pool.addLast(current);
+                current = current.left;
+            }
+
+        }
+        return 0;
+    }
+}
