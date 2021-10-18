f8b4dd061012f81d351b41ae2a57a25de1d0a424
LiZongjie <164260294@qq.com>
2021-10-18 14:58:46 +0800

    IMP 优化查找数组中第三大的数的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month10/week2/Z3ThirdMax.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month10/week2/Z3ThirdMax.java
@@ -14,44 +14,51 @@ package org.zongjieli.leetcode.question.daily.year2021.month10.week2;
 public class Z3ThirdMax {
 
     public int thirdMax(int[] nums) {
-        int[] value = new int[3];
-        int count = 1;
-        value[0] = nums[0];
+        int a = nums[0];
+        int b = Integer.MAX_VALUE;
+        int c = Integer.MAX_VALUE;
+
         for (int i = 1 ; i < nums.length ; i++){
-            if (nums[i] == value[0]){
-                continue;
-            }
-            if (count == 1){
-                count = 2;
-                value[1] = nums[i];
-                continue;
-            }
-            if (nums[i] == value[1]){
-                continue;
-            }
-            if (count == 2){
-                count = 3;
-                value[2] = nums[i];
-                continue;
-            }
-            if (value[2] == nums[i]){
+            if (nums[i] == a){
                 continue;
             }
 
-            int index = value[0] > value[1] ? 1 : 0;
-            index = value[index] > value[2] ? 2 : index;
-            if (value[index] < nums[i]){
-                value[index] = nums[i];
+            if (nums[i] > a){
+                c = b;
+                b = a;
+                a = nums[i];
+                continue;
+            }
+
+            if (b == Integer.MAX_VALUE){
+                b = nums[i];
+                continue;
+            }
+
+            if (b == nums[i]){
+                continue;
+            }
+
+            if (b < nums[i]){
+                c = b;
+                b = nums[i];
+                continue;
+            }
+
+            if (c == Integer.MAX_VALUE || c < nums[i]){
+                c = nums[i];
             }
         }
-        return count == 1 ? value[0] : (count == 2 ? Math.max(value[0],value[1]) : Math.min(Math.min(value[0],value[1]),value[2]));
+
+        return c == Integer.MAX_VALUE ? a : c;
     }
 
     public static void main(String[] args) {
         Z3ThirdMax test = new Z3ThirdMax();
+        System.out.println(test.thirdMax(new int[]{3,2,1}));
 //        System.out.println(test.thirdMax(new int[]{2,2,3,1}));
 //        System.out.println(test.thirdMax(new int[]{2,2,1}));
 //        System.out.println(test.thirdMax(new int[]{2,1,1}));
-        System.out.println(test.thirdMax(new int[]{1,2,2,5,3,5}));
+//        System.out.println(test.thirdMax(new int[]{1,2,2,5,3,5}));
     }
 }
