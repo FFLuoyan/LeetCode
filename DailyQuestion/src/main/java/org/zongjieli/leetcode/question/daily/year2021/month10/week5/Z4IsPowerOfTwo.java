9ae5171df901f1d474d1bb9fb106867bf4c089c4
LiZongjie <164260294@qq.com>
2021-10-28 11:16:30 +0800

    IMP 优化判断一个整数重新排列后是否为 2 的幂的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month10/week5/Z4IsPowerOfTwo.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month10/week5/Z4IsPowerOfTwo.java
@@ -1,9 +1,5 @@
 package org.zongjieli.leetcode.question.daily.year2021.month10.week5;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * 给定正整数 N,按任何顺序(包括原始顺序)将数字重新排序,注意其前导数字不能为零
  * 如果可以通过上述方式得到 2 的幂,返回 true,否则返回 false
@@ -14,27 +10,14 @@ import java.util.List;
  */
 public class Z4IsPowerOfTwo {
 
-    private long[][] values = new long[][]{
-            {}
-            ,{8, 4, 2, 1}
-            ,{64, 32, 61}
-            ,{521, 652, 821}
-            ,{9821, 9640, 8420, 4210}
-            ,{66553, 87632, 86431}
-            ,{885422, 644221, 732110}
-            ,{8888630, 9444310, 9752210, 8765410}
-            ,{88766410, 55443332, 77766211}
-            ,{987653210, 866554432, 877432211}
-            ,{9998855432L, 9997664422L, 8876444321L, 8774432110L}};
+    private long[] values = new long[]{1, 2, 4, 8, 32, 61, 64, 521, 652, 821, 4210, 8420, 9640, 9821, 66553, 86431, 87632, 644221, 732110, 885422, 8765410, 8888630, 9444310, 9752210, 55443332, 77766211, 88766410, 866554432, 877432211, 987653210, 8774432110L, 8876444321L};
 
     public boolean reorderedPowerOf2(int n) {
         int[] count = new int[10];
-        int bit = 0;
         while (n > 0){
             int last = n % 10;
             count[last]++;
             n /= 10;
-            bit++;
         }
         long max = 0;
         for (int i = 9 ; i >= 0 ; i--){
@@ -43,7 +26,7 @@ public class Z4IsPowerOfTwo {
                 max += i;
             }
         }
-        for (long l : values[bit]) {
+        for (long l : values) {
             if (max == l){
                 return true;
             }
