b9dfa3583902cf7124918ec305285c050cd1e5d4
LiZongjie <164260294@qq.com>
2021-11-04 16:20:02 +0800

    IMP 优化计算数组中最长递增子序列长度的算法

--- Origin/src/main/java/org/zongjieli/leetcode/origin/year2021/month11/LongestSub.java
+++ Origin/src/main/java/org/zongjieli/leetcode/origin/year2021/month11/LongestSub.java
@@ -11,18 +11,32 @@ package org.zongjieli.leetcode.origin.year2021.month11;
  */
 public class LongestSub {
     public int lengthOfLIS(int[] nums) {
-        int result = 1;
-        int[] length = new int[nums.length];
-        for (int i = 0 ; i < nums.length ; i++){
-            int currentLength = 1;
-            for (int j = 0 ; j < i ; j++){
-                if (nums[i] > nums[j]){
-                    currentLength = Math.max(length[j] + 1, currentLength);
+        int[] length = new int[2501];
+        int max = 1;
+        length[1] = nums[0];
+        for (int i = 1 ; i < nums.length ; i++){
+            int temp = max;
+            while (temp >= 1){
+                if (nums[i] > length[temp]){
+                    if (temp == max){
+                        length[++max] = nums[i];
+                    } else {
+                        length[temp + 1] = Math.min(length[temp + 1], nums[i]);
+                    }
+                    break;
                 }
+                temp--;
             }
-            length[i] = currentLength;
-            result = Math.max(result, currentLength);
+
+            if (temp == 0){
+                length[1] = nums[i];
+            }
         }
-        return result;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        LongestSub test = new LongestSub();
+        System.out.println(test.lengthOfLIS(new int[]{1,2,3,0,1,2,3,4}));
     }
 }
