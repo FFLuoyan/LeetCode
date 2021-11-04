204dcc476dfe91c7426cc600ac8c92f49d610161
LiZongjie <164260294@qq.com>
2021-11-04 16:49:42 +0800

    IMP 优化计算数组中最长递增子序列长度的算法

--- Origin/src/main/java/org/zongjieli/leetcode/origin/year2021/month11/LongestSub.java
+++ Origin/src/main/java/org/zongjieli/leetcode/origin/year2021/month11/LongestSub.java
@@ -12,24 +12,14 @@ package org.zongjieli.leetcode.origin.year2021.month11;
 public class LongestSub {
     public int lengthOfLIS(int[] nums) {
         int[] length = new int[2501];
-        int max = 1;
-        length[1] = nums[0];
-        for (int i = 1 ; i < nums.length ; i++){
-            int temp = max;
-            while (temp >= 1){
-                if (nums[i] > length[temp]){
-                    if (temp == max){
-                        length[++max] = nums[i];
-                    } else {
-                        length[temp + 1] = Math.min(length[temp + 1], nums[i]);
-                    }
+        int max = 0;
+        length[0] = Integer.MIN_VALUE;
+        for (int num : nums) {
+            for (int temp = max ; temp >= 0 ; temp--){
+                if (num > length[temp]) {
+                    length[temp == max ? ++max : temp + 1] = num;
                     break;
                 }
-                temp--;
-            }
-
-            if (temp == 0){
-                length[1] = nums[i];
             }
         }
         return max;
@@ -38,5 +28,7 @@ public class LongestSub {
     public static void main(String[] args) {
         LongestSub test = new LongestSub();
         System.out.println(test.lengthOfLIS(new int[]{1,2,3,0,1,2,3,4}));
+        System.out.println(test.lengthOfLIS(new int[]{10,9,2,5,3,7,101,18}));
+        System.out.println(test.lengthOfLIS(new int[]{-10,9,2,5,3,7,101,18}));
     }
 }
