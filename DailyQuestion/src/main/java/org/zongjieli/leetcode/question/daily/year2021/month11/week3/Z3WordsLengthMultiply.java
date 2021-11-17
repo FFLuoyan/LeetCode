b4b50c03ad538e4feaac9528b25799ddf38e439c
LiZongjie <164260294@qq.com>
2021-11-17 17:23:38 +0800

    ADD 完成计算单词数组中互相不包含相同字符的单词对的长度的积的最大值

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month11/week3/Z3WordsLengthMultiply.java
@@ -0,0 +1,42 @@
+package org.zongjieli.leetcode.question.daily.year2021.month11.week3;
+/**
+ * 给定一个字符串数组 words
+ * 找到 length(word[i]) * length(word[j]) 的最大值
+ * 并且这两个单词不含有公共字母
+ * 可以认为每个单词只包含小写字母
+ * 如果不存在这样的两个单词,返回 0
+ *
+ * 2 <= words.length <= 1000
+ * 1 <= words[i].length <= 1000
+ * words[i] 仅包含小写字母
+ *
+ * @author   Li.zongjie
+ * @date     2021/11/17
+ * @version  1.0
+ */
+public class Z3WordsLengthMultiply {
+    public int maxProduct(String[] words) {
+        boolean[][] exists = new boolean[words.length][26];
+        int result = 0;
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            boolean[] current = exists[i];
+            for (int j = 0 ; j < word.length() ; j++){
+                current[word.charAt(j) - 'a'] = true;
+            }
+            for (int j = 0 ; j < i ; j++){
+                boolean[] compare = exists[j];
+                int k = 0;
+                for (; k < 26 ; k++){
+                    if (compare[k] && current[k]){
+                        break;
+                    }
+                }
+                if (k == 26){
+                    result = Math.max(result, words[j].length() * word.length());
+                }
+            }
+        }
+        return result;
+    }
+}
