c03e182ae4d85df0f44a89f22c01aaa0665906e1
LiZongjie <164260294@qq.com>
2021-11-18 11:24:08 +0800

    ADD 完成 Map 中计算前缀和的重查询的算法(当查询较插入比较多时,效率较高)

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month11/week2/Z7PrefixMap.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month11/week2/Z7PrefixMap.java
@@ -2,7 +2,6 @@ package org.zongjieli.leetcode.question.daily.year2021.month11.week2;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * 实现一个 MapSum 类,支持两个方法,insert 和 sum
@@ -25,23 +24,80 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class Z7PrefixMap {
 
-    private Map<String, Integer> value = new HashMap<>();
+    /**
+     * 普通遍历方法
+     */
+    static class Z7PrefixMapSimple{
 
-    public Z7PrefixMap() {
+        private Map<String, Integer> value = new HashMap<>();
+        private int[] result = new int[]{0};
 
+        public Z7PrefixMapSimple() {
+
+        }
+
+        public void insert(String key, int val) {
+            value.put(key,val);
+        }
+
+        public int sum(String prefix) {
+            result[0] = 0;
+            value.forEach((k,v) -> {
+                if (k.startsWith(prefix)){
+                    result[0] += v;
+                }
+            });
+            return result[0];
+        }
     }
 
-    public void insert(String key, int val) {
-        value.put(key,val);
-    }
+    /**
+     * 前缀保留方法,每次插入时将前缀和计算完成
+     * 使得插入时的时间消耗增加,但获取结果时非常迅速
+     */
+    static class Z7PrefixMapPrefix{
+        private Map<String, Integer> valueMap = new HashMap<>();
+        private Map<String, Integer> prefixMap = new HashMap<>();
 
-    public int sum(String prefix) {
-        AtomicInteger result = new AtomicInteger();
-        value.forEach((k,v) -> {
-            if (k.startsWith(prefix)){
-                result.addAndGet(v);
+        public Z7PrefixMapPrefix() {
+
+        }
+
+        public void insert(String key, int val) {
+            Integer save = valueMap.get(key);
+
+            if (save == null){
+                // value
+                valueMap.put(key, val);
+
+                // prefix
+                for (int i = 1 ; i <= key.length() ; i++){
+                    String tempKey = key.substring(0, i);
+                    prefixMap.merge(tempKey, val, Integer::sum);
+                }
+            } else if (save != val){
+                // value
+                valueMap.put(key, val);
+
+                // prefix
+                int add = val - save;
+                for (int i = 1 ; i <= key.length() ; i++){
+                    String tempKey = key.substring(0, i);
+                    Integer v = prefixMap.get(tempKey);
+                    prefixMap.put(tempKey, v + add);
+                }
             }
-        });
-        return result.get();
+
+        }
+
+        public int sum(String prefix) {
+            return prefixMap.getOrDefault(prefix, 0);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Z7PrefixMapPrefix prefixTest = new Z7PrefixMapPrefix();
+        prefixTest.insert("apple", 3);
     }
 }
