ce3e86c160ab9d583d852d6ce4f39962e32dcb8d
LiZongjie <164260294@qq.com>
2021-12-12 23:26:59 +0800

    ADD 完成将字符串中大写字母转换成相同的小写字母的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month12/week2/Z7UpperToLower.java
@@ -0,0 +1,16 @@
+package org.zongjieli.leetcode.question.daily.year2021.month12.week2;
+/**
+ * 给定一个字符串 s,将该字符串中的大写字母转换成相同的小写字母,返回新的字符串
+ *
+ * 1 <= s.length <= 100
+ * s 由 ASCII 字符集中的可打印字符组成
+ *
+ * @author   Li.zongjie
+ * @date     2021/12/12
+ * @version  1.0
+ */
+public class Z7UpperToLower {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
