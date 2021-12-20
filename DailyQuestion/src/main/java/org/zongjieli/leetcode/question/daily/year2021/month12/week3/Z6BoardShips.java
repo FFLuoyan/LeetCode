e3a0b32c8f8f911a7b84175a8e2a33e1cd1e1ad5
li·zongjie <164260294@qq.com>
2021-12-20 14:51:13 +0800

    IMP 优化计算甲板上的战舰数量的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month12/week3/Z6BoardShips.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month12/week3/Z6BoardShips.java
@@ -20,20 +20,9 @@ package org.zongjieli.leetcode.question.daily.year2021.month12.week3;
 public class Z6BoardShips {
     public int countBattleships(char[][] board) {
         int count = 0;
-        if (board[0][0] == 'X'){
-            count++;
-        }
-        for (int i = 1 ; i < board[0].length ; i++){
-            if (board[0][i] == 'X' && board[0][i - 1] != 'X'){
-                count++;
-            }
-        }
-        for (int i = 1 ; i < board.length ; i++){
-            if (board[i][0] == 'X' && board[i - 1][0] != 'X'){
-                count++;
-            }
-            for (int j = 1 ; j < board[i].length ; j++){
-                if (board[i][j] == 'X' && board[i - 1][j] != 'X' && board[i][j - 1] != 'X'){
+        for (int i = 0 ; i < board.length ; i++){
+            for (int j = 0 ; j < board[i].length ; j++){
+                if (board[i][j] == 'X' && (i == 0 || board[i - 1][j] != 'X') && (j == 0 || board[i][j - 1] != 'X')){
                     count++;
                 }
             }
