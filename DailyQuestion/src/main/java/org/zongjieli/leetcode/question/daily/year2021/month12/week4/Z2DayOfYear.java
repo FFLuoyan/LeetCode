a23c1e1b7b1f169b1c696b6fc111e948e42f1d62
li·zongjie <164260294@qq.com>
2021-12-21 09:54:41 +0800

    IMP 优化计算指定日期为当年的多少天的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month12/week4/Z2DayOfYear.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2021/month12/week4/Z2DayOfYear.java
@@ -1,9 +1,5 @@
 package org.zongjieli.leetcode.question.daily.year2021.month12.week4;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-
 /**
  * 给定一个字符串 date,按 YYYY-MM-DD 格式表示一个现行公元纪年法日期
  * 请计算并返回该日期是当年的第几天
@@ -21,13 +17,30 @@ import java.util.Calendar;
  */
 public class Z2DayOfYear {
     public int dayOfYear(String date) {
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        Calendar calendar = Calendar.getInstance();
-        try {
-            calendar.setTimeInMillis(format.parse(date).getTime());
-        } catch (ParseException e) {
-            e.printStackTrace();
+        byte[] dateValue = date.getBytes();
+        int year = (dateValue[0] - '0') * 1000 + (dateValue[1] - '0') * 100 + (dateValue[2] - '0') * 10 + dateValue[3] - '0';
+        int month = (dateValue[5] - '0') * 10 + dateValue[6] - '0';
+        int day = (dateValue[8] - '0') * 10 + dateValue[9] - '0';
+        int before = (year & 3) == 0 && (year % 100 != 0 || year % 400 == 0) ? 1 : 0;
+        switch (month){
+            case 1: return day;
+            case 2: return 31 + day;
+            case 3: return before + 59 + day;
+            case 4: return before + 90 + day;
+            case 5: return before + 120 + day;
+            case 6: return before + 151 + day;
+            case 7: return before + 181 + day;
+            case 8: return before + 212 + day;
+            case 9: return before + 243 + day;
+            case 10: return before + 273 + day;
+            case 11: return before + 304 + day;
+            case 12: return before + 334 + day;
+            default: return 0;
         }
-        return calendar.get(Calendar.DAY_OF_YEAR);
+    }
+
+    public static void main(String[] args) {
+        Z2DayOfYear test = new Z2DayOfYear();
+        System.out.println(test.dayOfYear("2019-01-09"));
     }
 }
