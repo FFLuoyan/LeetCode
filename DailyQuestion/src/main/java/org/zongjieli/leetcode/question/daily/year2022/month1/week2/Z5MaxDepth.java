bd04e9582678e661564281d6fb7fa628f72e1f61
li·zongjie <164260294@qq.com>
2022-01-07 11:31:16 +0800

    ADD 完成计算括号表达式的最大嵌套深度的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month1/week2/Z5MaxDepth.java
@@ -0,0 +1,43 @@
+package org.zongjieli.leetcode.question.daily.year2022.month1.week2;
+/**
+ * 如果字符串满足以下条件之一,则可以称之为有效括号字符串(valid parentheses string,可以简写为 VPS):
+ *  字符串是一个空字符串 "",或者是一个不为 "(" 或 ")" 的单字符
+ *  字符串可以写为 AB(A 与 B 字符串连接),其中 A 和 B 都是有效括号字符串
+ *  字符串可以写为 (A),其中 A 是一个有效括号字符串
+ *
+ * 类似地,可以定义任何有效括号字符串 S 的 嵌套深度 depth(S):
+ *  depth("") = 0
+ *  depth(C) = 0,其中 C 是单个字符的字符串,且该字符不是 "(" 或者 ")"
+ *  depth(A + B) = max(depth(A), depth(B)),其中 A 和 B 都是有效括号字符串
+ *  depth("(" + A + ")") = 1 + depth(A),其中 A 是一个有效括号字符串
+ *
+ * 例如：""、"()()"、"()(()())" 都是有效括号字符串(嵌套深度分别为 0、1、2)
+ * 而 ")(" 、"(()" 都不是有效括号字符串
+ *
+ * 给定一个有效括号字符串 s,返回该字符串的 s 嵌套深度
+ *
+ *  1 <= s.length <= 100
+ *  s 由数字 0-9 和字符 '+'、'-'、'*'、'/'、'('、')' 组成
+ *
+ * 题目数据保证括号表达式 s 是 有效的括号表达式
+ *
+ * @author   Li.zongjie
+ * @date     2022/1/7
+ * @version  1.0
+ */
+public class Z5MaxDepth {
+    public int maxDepth(String s) {
+        int max = 0;
+        int current = 0;
+        for (int i = 0 ; i < s.length() ; i++){
+            char c = s.charAt(i);
+            if (c == '('){
+                current++;
+                max = Math.max(current, max);
+            } else if (c == ')'){
+                current--;
+            }
+        }
+        return max;
+    }
+}
