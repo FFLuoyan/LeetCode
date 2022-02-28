b3677f710e2495f0a18250b592d74a3bc8b7171a
li·zongjie <164260294@qq.com>
2022-02-28 11:05:42 +0800

    IMP 优化生成最优除法的字符串的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month2/week4/Z7OptimalDivision.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month2/week4/Z7OptimalDivision.java
@@ -20,11 +20,13 @@ public class Z7OptimalDivision {
         if (nums.length == 1) {
             return String.valueOf(nums[0]);
         }
-        if (nums.length == 2) {
-            return nums[0] + "/" + nums[1];
-        }
         StringBuilder result = new StringBuilder();
-        result.append(nums[0]).append('/').append('(').append(nums[1]);
+        result.append(nums[0]).append('/');
+        if (nums.length == 2) {
+            result.append(nums[1]);
+            return result.toString();
+        }
+        result.append('(').append(nums[1]);
         for (int i = 2 ; i < nums.length ; i++) {
             result.append('/').append(nums[i]);
         }
