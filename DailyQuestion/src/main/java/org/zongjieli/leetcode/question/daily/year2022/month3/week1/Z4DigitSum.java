560ee66150a6818d8825e0ec4e303a3dca95b0fa
li·zongjie <164260294@qq.com>
2022-03-04 13:34:23 +0800

    IMP 优化求反复数位和的算法的注释

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month3/week1/Z4DigitSum.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month3/week1/Z4DigitSum.java
@@ -9,18 +9,16 @@ package org.zongjieli.leetcode.question.daily.year2022.month3.week1;
 public class Z4DigitSum {
     public int addDigits(int num) {
         /*
-            对于给定的数 x,可以表达为
-            a1 + a2 * (10 - 1 + 1) + a3 * (100 - 1 + 1) + ... + an * (10^n - 1 + 1)
-            即 a1 + a2 + a3 + ... + an + 9 * a2 + 99 * a3 + ... + (10^n - 1) * an
-            已知其其数位和 f(x) 为 a1 + a2 + a3 + ... + an
-            则 x 可以表示为 f(x) + 9 * t
-            当 f(x) < 9 时
-                结果 g(x) = f(x) = x % 9
-            当 f(x) = 9 时
-                g(x) = 9
-            当 f^n(x) > 9 时
-                有 f^(n - 1)(x) = 9 * t + f^n(x)
-                故 g(x) = f^n(x) = f^(n - 1)(x) % 9 = x % 9 (取余结果为 0 时,g(x) = 9)
+            设对于任意给定 x,其各位之和为 f(x)
+            对于任意 x 可以表达为
+            x = a1 + a2 * 10 + a3 * 100 + ... + an * 10^(n - 1)
+              = a1 + a2 + 9 * a2 + a3 + 99 * a3 + an + (10^(n - 1) - 1) * an
+              = f(x) + 9 * t
+            f(x) = x - 9 * t
+            即对 x 求各位之和,可以视为从 x 中减去一个数,这个数是 9 的倍数
+            当这个操作重复执行,直至 f(x) 的结果为个位数,总共减去的值依旧为 9 的倍数
+            所以对于结果 r(x) 来说
+            有 r(x) = f^n(x) = x - 9 * q
          */
         return (num - 1) % 9 + 1;
     }
