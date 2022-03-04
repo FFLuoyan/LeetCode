605b9abfa7bafc473a08ad9cd3f23f7148bdcac2
li·zongjie <164260294@qq.com>
2022-03-04 13:39:44 +0800

    ADD 完成计算子数组范围和的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month3/week1/Z5SubArraySum.java
@@ -0,0 +1,29 @@
+package org.zongjieli.leetcode.question.daily.year2022.month3.week1;
+/**
+ * 给定一个整数数组 nums
+ * nums 中子数组的范围是子数组中最大元素和最小元素的差值
+ * 返回 nums 中所有子数组范围的和
+ * 子数组是数组中一个连续非空的元素序列
+ *
+ * 1 <= nums.length <= 1000
+ * -10^9 <= nums[i] <= 10^9
+ *
+ * @author   Li.zongjie
+ * @date     2022/3/4
+ * @version  1.0
+ */
+public class Z5SubArraySum {
+    public long subArrayRanges(int[] nums) {
+        long result = 0;
+        for (int start = 0 ; start < nums.length ; start++) {
+            int min = nums[start];
+            int max = min;
+            for (int end = start + 1 ; end < nums.length ; end++) {
+                min = Math.min(min, nums[end]);
+                max = Math.max(max, nums[end]);
+                result += (max - min);
+            }
+        }
+        return result;
+    }
+}
