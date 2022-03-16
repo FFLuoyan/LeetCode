040c43c9b3aca1012ba34ed3e37a4d7203292dcc
li·zongjie <164260294@qq.com>
2022-03-16 14:22:20 +0800

    IMP 优化查找有序数组中唯一的未成对的数字的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month2/week3/Z1SingleNumber.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month2/week3/Z1SingleNumber.java
@@ -17,33 +17,16 @@ public class Z1SingleNumber {
     public int singleNonDuplicate(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
-        while (left <= right) {
-            if (left == right) {
-                return nums[left];
-            }
+        while (left < right) {
             int middle = (left + right) / 2;
-            int size = middle - left;
-            if (size % 2 == 1) {
-                // 单边单数
-                if (nums[middle] == nums[middle + 1]) {
-                    right = middle - 1;
-                } else if (nums[middle] == nums[middle - 1]){
-                    left = middle + 1;
-                } else {
-                    return nums[middle];
-                }
+            boolean isOdd = (middle - left) % 2 == 1;
+            if (nums[middle] == nums[isOdd ? middle + 1 : middle - 1]) {
+                right = isOdd ? middle - 1 : middle - 2;
             } else {
-                // 单边双数
-                if (nums[middle] == nums[middle + 1]) {
-                    left = middle +  2;
-                } else if (nums[middle] == nums[middle - 1]){
-                    right = middle - 2;
-                } else {
-                    return nums[middle];
-                }
+                left = isOdd ? middle + 1 : middle;
             }
         }
-        return 0;
+        return nums[left];
     }
 
     public static void main(String[] args) {
@@ -54,6 +37,8 @@ public class Z1SingleNumber {
         System.out.println(test.singleNonDuplicate(new int[]{1, 1, 2, 3, 3, 4, 4, 8, 8}));
         // 2
         System.out.println(test.singleNonDuplicate(new int[]{1, 1, 2, 3, 3, 4, 4, 8, 8, 9, 9}));
+        // 10
+        System.out.println(test.singleNonDuplicate(new int[]{3, 3, 7, 7, 10, 11, 11}));
     }
 
 }
