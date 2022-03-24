38c00ae630cf5c8b0ac96ed4b01f321ceffc824a
li·zongjie <164260294@qq.com>
2022-03-24 10:32:32 +0800

    IMP 优化计算灰度的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month3/week4/Z4ImageSmooth.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month3/week4/Z4ImageSmooth.java
@@ -26,58 +26,69 @@ public class Z4ImageSmooth {
     public int[][] imageSmoother(int[][] img) {
         int m = img.length;
         int n = img[0].length;
+        if (m == 1) {
+            if (n == 1) {
+                return img;
+            }
+            int[] row = new int[n];
+            int[] ir = img[0];
+            row[0] = sum0(ir) / 2;
+            row[n - 1] = sumN(ir, n) / 2;
+            for (int i = 1 ; i < n - 1 ; i++) {
+                row[i] = sum(ir, i) / 3;
+            }
+            return new int[][]{row};
+        }
         int[][] result = new int[m][n];
-        for (int i = 0 ; i < m ; i++) {
-            int[] rr = result[i];
-            int[] irb = null;
-            if (i > 0) {
-                irb = img[i - 1];
+        if (n == 1) {
+            result[0][0] = (img[0][0] + img[1][0]) / 2;
+            result[m - 1][0] = (img[m - 2][0] + img[m - 1][0]) / 2;
+            for (int i = 1 ; i < m - 1 ; i++) {
+                result[i][0] = (img[i - 1][0] + img[i][0] + img[i + 1][0]) / 3;
             }
-            int[] irc = img[i];
-            int[] ira = null;
-            if (i < m - 1) {
-                ira = img[i + 1];
-            }
+            return result;
+        }
 
-            for (int j = 0 ; j < n ; j++) {
-                int count = 1;
-                if (i > 0) {
-                    count++;
-                    if (j > 0) {
-                        rr[j] += irb[j - 1];
-                        count++;
-                    }
-                    rr[j] += irb[j];
-                    if (j < n - 1) {
-                        rr[j] += irb[j + 1];
-                        count++;
-                    }
-                }
-                if (j > 0) {
-                    rr[j] += irc[j - 1];
-                    count++;
-                }
-                rr[j] += irc[j];
-                if (j < n - 1) {
-                    rr[j] += irc[j + 1];
-                    count++;
-                }
-                if (i < m - 1) {
-                    count++;
-                    if (j > 0) {
-                        rr[j] += ira[j - 1];
-                        count++;
-                    }
-                    rr[j] += ira[j];
-                    if (j < n - 1) {
-                        rr[j] += ira[j + 1];
-                        count++;
-                    }
-                }
-                rr[j] /= count;
+        int[] rf = result[0];
+        int[] irfc = img[0];
+        int[] irfa = img[1];
+        rf[0] = (sum0(irfc) + sum0(irfa)) / 4;
+        rf[n - 1] = (sumN(irfc, n) + sumN(irfa, n)) / 4;
+        for (int i = 1 ; i < n - 1 ; i++) {
+            rf[i] = (sum(irfc, i) + sum(irfa, i)) / 6;
+        }
+
+        rf = result[m - 1];
+        irfc = img[m - 1];
+        int[] irfb = img[m - 2];
+        rf[0] = (sum0(irfc) + sum0(irfb)) / 4;
+        rf[n - 1] = (sumN(irfc, n) + sumN(irfb, n)) / 4;
+        for (int i = 1 ; i < n - 1 ; i++) {
+            rf[i] = (sum(irfc, i) + sum(irfb, i)) / 6;
+        }
+
+        for (int i = 1 ; i < m - 1 ; i++) {
+            rf = result[i];
+            irfb = img[i - 1];
+            irfc = img[i];
+            irfa = img[i + 1];
+            rf[0] = (sum0(irfb) + sum0(irfc) + sum0(irfa)) / 6;
+            rf[n - 1] = (sumN(irfb, n) + sumN(irfc, n) + sumN(irfa, n)) / 6;
+            for (int j = 1 ; j < n - 1 ; j++) {
+                rf[j] = (sum(irfb, j) + sum(irfc, j) + sum(irfa, j)) / 9;
             }
         }
         return result;
+    }
+
+    private int sum0(int[] r) {
+        return r[0] + r[1];
+    }
+    private int sumN(int[] r, int n) {
+        return r[n - 2] + r[n - 1];
+    }
+    private int sum(int[] r, int i) {
+        return r[i - 1] + r[i] + r[i + 1];
     }
 
     public static void main(String[] args) {
