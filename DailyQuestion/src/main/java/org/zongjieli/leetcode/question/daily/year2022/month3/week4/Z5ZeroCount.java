5cf169cc9e1b5db7e2b632572fdd0eff32279958
li·zongjie <164260294@qq.com>
2022-03-25 16:59:54 +0800

    IMP 优化计算阶乘中尾部 0 的数量的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month3/week4/Z5ZeroCount.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month3/week4/Z5ZeroCount.java
@@ -11,18 +11,23 @@ package org.zongjieli.leetcode.question.daily.year2022.month3.week4;
 public class Z5ZeroCount {
 
     public int trailingZeroes(int n) {
-        if (n < 25) {
-            return n / 5;
+        if (n < 5) {
+            return 0;
         }
-        if (n < 125) {
-            return (n / 25) * 6 + trailingZeroes(n % 25);
+        int count = 1;
+        int base = 5;
+        while (n >= base) {
+            count = 5 * count + 1;
+            base *= 5;
         }
-        if (n < 625) {
-            return (n / 125) * 31 + trailingZeroes(n % 125);
-        }
-        if (n < 3125) {
-            return (n / 625) * 156 + trailingZeroes(n % 625);
-        }
-        return (n / 3125) * 781 + trailingZeroes(n % 3125);
+        base /= 5;
+        count = (count - 1) / 5;
+        return (n / base) * count + trailingZeroes(n % base);
+    }
+
+    public static void main(String[] args) {
+        Z5ZeroCount test = new Z5ZeroCount();
+        // 6
+        System.out.println(test.trailingZeroes(25));
     }
 }
