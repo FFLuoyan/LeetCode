bbe979e5fc7d84899e3dd9b158d40c9b4647a655
li·zongjie <164260294@qq.com>
2022-03-28 14:14:17 +0800

    ADD 完成比赛记分的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month3/week4/Z6BaseballGame.java
@@ -0,0 +1,58 @@
+package org.zongjieli.leetcode.question.daily.year2022.month3.week4;
+
+import java.util.LinkedList;
+
+/**
+ * 一场采用特殊赛制的棒球比赛,这场比赛由若干回合组成
+ * 过去几回合的得分可能会影响以后几回合的得分
+ *
+ * 比赛开始时,记录是空白的,存在一个记录操作的字符串列表 ops
+ * 其中 ops[i] 是需要记录的第 i 项操作,ops 遵循下述规则:
+ *
+ *  整数 x: 表示本回合新获得分数 x
+ *  "+":    表示本回合新获得的得分是前两次得分的总和,数据保证记录此操作时前面总是存在两个有效的分数
+ *  "D":    表示本回合新获得的得分是前一次得分的两倍,数据保证记录此操作时前面总是存在一个有效的分数
+ *  "C":    表示前一次得分无效,将其从记录中移除,数据保证记录此操作时前面总是存在一个有效的分数
+ *
+ * 请返回记录中所有得分的总和
+ *
+ * 1 <= ops.length <= 1000
+ * ops[i] 为 "C"、"D"、"+",或者一个表示整数的字符串,整数范围是 [-3 * 10^4, 3 * 10^4]
+ * 对于 "+" 操作,题目数据保证记录此操作时前面总是存在两个有效的分数
+ * 对于 "C" 和 "D" 操作,题目数据保证记录此操作时前面总是存在一个有效的分数
+ *
+ * @author   Li.zongjie
+ * @date     2022/3/28
+ * @version  1.0
+ */
+public class Z6BaseballGame {
+
+    public int calPoints(String[] ops) {
+        LinkedList<Integer> points = new LinkedList<>();
+        int all = 0;
+        for (String op : ops) {
+            if ("D".equals(op)) {
+                int p = 2 * points.getFirst();
+                all += p;
+                points.addFirst(p);
+            } else if ("+".equals(op)) {
+                int p = points.getFirst() + points.get(1);
+                all += p;
+                points.addFirst(p);
+            } else if ("C".equals(op)) {
+                all -= points.pollFirst();
+            } else {
+                int p = Integer.parseInt(op);
+                all += p;
+                points.addFirst(p);
+            }
+        }
+        return all;
+    }
+
+    public static void main(String[] args) {
+        Z6BaseballGame test = new Z6BaseballGame();
+        // 30
+        System.out.println(test.calPoints(new String[]{"5", "2", "C", "D", "+"}));
+    }
+}
