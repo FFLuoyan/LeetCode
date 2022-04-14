6ad23b081d606dd0123fc330f7d05d33e13dd7d9
li·zongjie <164260294@qq.com>
2022-04-14 15:50:34 +0800

    IMP 优化查询有序字符列表中比目标字符大(循环比较)的最小字符的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month4/week1/Z7MaxChar.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month4/week1/Z7MaxChar.java
@@ -21,13 +21,19 @@ package org.zongjieli.leetcode.question.daily.year2022.month4.week1;
 public class Z7MaxChar {
 
     public char nextGreatestLetter(char[] letters, char target) {
-        char result = letters[0];
-        for (char letter : letters) {
-            if (letter > target) {
-                return letter;
+        int left = 0, right = letters.length - 1, middle;
+        if (letters[right] <= target) {
+            return letters[0];
+        }
+        while (left < right) {
+            middle = (left + right) / 2;
+            if (letters[middle] <= target) {
+                left = middle + 1;
+            } else {
+                right = middle;
             }
         }
-        return result;
+        return letters[left];
     }
 
     public static void main(String[] args) {
