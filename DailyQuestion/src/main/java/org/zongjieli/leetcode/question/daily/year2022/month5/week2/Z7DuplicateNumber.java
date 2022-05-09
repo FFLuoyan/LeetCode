a2784726d4a5922665b3f543cc215bdd92921b72
li·zongjie <164260294@qq.com>
2022-05-09 17:19:18 +0800

    ADD 完成不需要额外空间且时间复杂度为 O(n) 的查找数组中出现两次的数字的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month5/week2/Z7DuplicateNumber.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month5/week2/Z7DuplicateNumber.java
@@ -32,4 +32,17 @@ public class Z7DuplicateNumber {
         }
         return result;
     }
+
+    public List<Integer> findDuplicatesWithoutExtra(int[] nums) {
+        List<Integer> result = new ArrayList<>(nums.length);
+        for (int i = 0 ; i < nums.length ; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0) {
+                result.add(index + 1);
+            } else {
+                nums[index] = -nums[index];
+            }
+        }
+        return result;
+    }
 }
