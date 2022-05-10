6e6547b8a05195da675e99472ac9477c4837e67f
li·zongjie <164260294@qq.com>
2022-05-10 09:31:16 +0800

    IMP 优化计算积小于目标值的连续子数组的数目的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month5/week2/Z4SubArrayProduct.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month5/week2/Z4SubArrayProduct.java
@@ -16,11 +16,11 @@ public class Z4SubArrayProduct {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
         int result = 0, end = 0, product = 1;
         for (int start = 0 ; start < nums.length ; start++) {
-            end = Math.max(start, end);
             while (end < nums.length && product < k) {
                 product *= nums[end++];
             }
-            result += Math.max(product < k ? end - start : end - start - 1, 0);
+            end = Math.max(end, start + 1);
+            result += product < k ? end - start : end - start - 1;
             product /= nums[start];
         }
         return result;
@@ -29,9 +29,9 @@ public class Z4SubArrayProduct {
     public static void main(String[] args) {
         Z4SubArrayProduct test = new Z4SubArrayProduct();
         // 8
-//        System.out.println(test.numSubarrayProductLessThanK(new int[]{10, 5, 2, 6}, 100));
-//        // 18
-//        System.out.println(test.numSubarrayProductLessThanK(new int[]{10, 9, 10, 4, 3, 8, 3, 3, 6, 2, 10, 10, 9, 3}, 19));
+        System.out.println(test.numSubarrayProductLessThanK(new int[]{10, 5, 2, 6}, 100));
+        // 18
+        System.out.println(test.numSubarrayProductLessThanK(new int[]{10, 9, 10, 4, 3, 8, 3, 3, 6, 2, 10, 10, 9, 3}, 19));
         // 0
         System.out.println(test.numSubarrayProductLessThanK(new int[]{1, 2, 3}, 0));
     }
