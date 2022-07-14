d270a7584b3d4f17094dd3e231f63696c3729e5f
li·zongjie <164260294@qq.com>
2022-07-14 09:52:54 +0800

    ADD 完成计算小于给定整数 n 的素数的数目的算法

--- Fun/src/main/java/org/zongjieli/leetcode/fun/year2022/month6/CountPrime.java
+++ Fun/src/main/java/org/zongjieli/leetcode/fun/year2022/month6/CountPrime.java
@@ -1,8 +1,5 @@
 package org.zongjieli.leetcode.fun.year2022.month6;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 给定整数 n,返回所有小于非负整数 n 的质数的数量
  *
@@ -15,23 +12,22 @@ import java.util.List;
 public class CountPrime {
 
     public int countPrimes(int n) {
-        if (n <= 2) {
-            return 0;
-        }
-        List<Integer> primes = new ArrayList<>();
-        primes.add(2);
-        a: for (int i = 3 ; i < n ; i++) {
-            int max = (int) Math.sqrt(i);
-            for (int prime : primes) {
-                if (prime > max) {
-                    primes.add(i);
-                    continue a;
-                }
-                if (i % prime == 0) {
-                    continue a;
+        int count = 0;
+        boolean[] save = new boolean[n];
+        for (int i = 2 ; i < n ; i++) {
+            if (!save[i]) {
+                count++;
+                for (int j = 2 * i ; j < n ; j += i) {
+                    save[j] = true;
                 }
             }
         }
-        return primes.size();
+        return count;
+    }
+
+    public static void main(String[] args) {
+        CountPrime test = new CountPrime();
+        // 4
+        System.out.println(test.countPrimes(10));
     }
 }
