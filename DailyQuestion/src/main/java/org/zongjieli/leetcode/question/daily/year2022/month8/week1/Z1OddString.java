cc69231d3a631967247a5c4fd3c0bf1ac0b23e06
li·zongjie <164260294@qq.com>
2022-08-01 14:50:08 +0800

    ADD 完成生成所有字符个数均为奇数的字符串的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month8/week1/Z1OddString.java
@@ -0,0 +1,28 @@
+package org.zongjieli.leetcode.question.daily.year2022.month8.week1;
+/**
+ * 给定一个整数 n,请返回一个含 n 个字符的字符串
+ * 其中每种字符在该字符串中都恰好出现奇数次
+ * 返回的字符串必须只含小写英文字母
+ * 如果存在多个满足题目要求的字符串
+ * 则返回其中任意一个即可
+ *
+ * 1 <= n <= 500
+ *
+ * @author   Li.zongjie
+ * @date     2022/8/1
+ * @version  1.0
+ */
+public class Z1OddString {
+
+    public String generateTheString(int n) {
+        int ac = (n & 1) == 1 ? n : n - 1;
+        StringBuilder result = new StringBuilder(n);
+        for (int i = 0 ; i < ac ; i++) {
+            result.append('a');
+        }
+        if (ac < n) {
+            result.append('b');
+        }
+        return result.toString();
+    }
+}
