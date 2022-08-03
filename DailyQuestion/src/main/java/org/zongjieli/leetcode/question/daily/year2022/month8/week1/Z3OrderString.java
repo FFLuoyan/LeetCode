046b136867bfef0f4c994e3a873e3899634129a3
li·zongjie <164260294@qq.com>
2022-08-03 09:40:33 +0800

    ADD 完成经过移动后可生成的字符串中字典序最小的字符串的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month8/week1/Z3OrderString.java
@@ -0,0 +1,46 @@
+package org.zongjieli.leetcode.question.daily.year2022.month8.week1;
+
+import java.util.Arrays;
+
+/**
+ * 给定一个字符串 s 和一个整数 k
+ * 可以从 s 的前 k 个字母中选择一个,并把它加到字符串的末尾
+ * 返回在应用上述步骤的任意数量的移动后,字典上最小的字符串
+ *
+ * 1 <= k <= S.length <= 1000
+ * s 只由小写字母组成。
+ *
+ * @author   Li.zongjie
+ * @date     2022/8/3
+ * @version  1.0
+ */
+public class Z3OrderString {
+
+    public String orderlyQueue(String s, int k) {
+        if (k == 1) {
+            int l = s.length();
+            char[] sRepeat = new char[2 * l];
+            for (int i = 0 ; i < l ; i++) {
+                char cc = s.charAt(i);
+                sRepeat[i] = cc;
+                sRepeat[i + l] = cc;
+            }
+            String result = s;
+            for (int i = 1 ; i < l ; i++) {
+                String compare = new String(sRepeat, i, l);
+                if (compare.compareTo(result) < 0) {
+                    result = compare;
+                }
+            }
+            return result;
+        }
+        char[] result = s.toCharArray();
+        Arrays.sort(result);
+        return new String(result);
+    }
+
+    public static void main(String[] args) {
+        Z3OrderString test = new Z3OrderString();
+        System.out.println(test.orderlyQueue("cba", 1));
+    }
+}
