5159640e3aa702882b5d6e84f1b4ae4598b4767e
li·zongjie <164260294@qq.com>
2022-08-04 14:49:09 +0800

    ADD 完成查找数据中和大于数组总和一半的最小长度最大总和的子序列的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month8/week1/Z4NonIncreaseSub.java
@@ -0,0 +1,43 @@
+package org.zongjieli.leetcode.question.daily.year2022.month8.week1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给定一个数组 nums,请从中抽取一个子序列
+ * 满足该子序列的元素之和严格大于未包含在该子序列中的各元素之和
+ * 如果存在多个解决方案,只需返回长度最小的子序列
+ * 如果仍然有多个解决方案,则返回元素之和最大的子序列
+ * 与子数组不同的地方在于,数组的子序列不强调元素在原数组中的连续性
+ * 也就是说,它可以通过从数组中分离一些(也可能不分离)元素得到
+ * 注意,题目数据保证满足所有约束条件的解决方案是唯一的
+ * 同时,返回的答案应当按非递增顺序排列
+ *
+ * 1 <= nums.length <= 500
+ * 1 <= nums[i] <= 100
+ *
+ * @author   Li.zongjie
+ * @date     2022/8/4
+ * @version  1.0
+ */
+public class Z4NonIncreaseSub {
+
+    public List<Integer> minSubsequence(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        int half = sum / 2;
+        sum = 0;
+        List<Integer> result = new ArrayList<>();
+        for (int i = nums.length - 1; i >= 0; i--) {
+            result.add(nums[i]);
+            if ((sum += nums[i]) > half) {
+                break;
+            }
+        }
+        return result;
+    }
+}
