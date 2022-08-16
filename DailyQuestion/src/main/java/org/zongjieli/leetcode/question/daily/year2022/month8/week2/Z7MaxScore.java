4c0fd21d6e82708734a173d7afb2a01de72d9539
li·zongjie <164260294@qq.com>
2022-08-16 16:40:06 +0800

    ADD 完成判断分割字符串可获最大得分的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month8/week2/Z7MaxScore.java
@@ -0,0 +1,28 @@
+package org.zongjieli.leetcode.question.daily.year2022.month8.week2;
+/**
+ * 给定一个由若干 0 和 1 组成的字符串 s
+ * 请计算并返回将该字符串分割成两个非空子字符串(即左子字符串和右子字符串)所能获得的最大得分
+ * 分割字符串的得分为左子字符串中 0 的数量加上右子字符串中 1 的数量
+ *
+ * 2 <= s.length <= 500
+ * 字符串 s 仅由字符 '0' 和 '1' 组成
+ *
+ * @author   Li.zongjie
+ * @date     2022/8/16
+ * @version  1.0
+ */
+public class Z7MaxScore {
+    public int maxScore(String s) {
+        int base = '1' - s.charAt(0), compare = 0, mc = 0;
+        for (int i = 1 ; i < s.length() - 1 ; i++) {
+            if (s.charAt(i) == '0') {
+                compare++;
+                mc = Math.max(compare, mc);
+            } else {
+                base++;
+                compare--;
+            }
+        }
+        return base + mc + s.charAt(s.length() - 1) - '0';
+    }
+}
