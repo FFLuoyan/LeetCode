9ba00c1323d40ac4058ab32a129fbac7cb939698
li·zongjie <164260294@qq.com>
2022-10-31 11:33:37 +0800

    ADD 完成计算神奇字符串中前 n 位 1 的出现次数的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month10/week6/Z1AmazingString.java
@@ -0,0 +1,50 @@
+package org.zongjieli.leetcode.question.daily.year2022.month10.week6;
+
+import java.util.LinkedList;
+
+/**
+ * 神奇字符串 s 仅由 '1' 和 '2' 组成,并需要遵守下面的规则:
+ *  神奇字符串 s 的神奇之处在于,串联字符串中 '1' 和 '2' 的连续出现次数可以生成该字符串
+ *  s 的前几个元素是 s = "1221121221221121122……"
+ *  如果将 s 中连续的若干 1 和 2 进行分组可以得到 "1 22 11 2 1 22 1 22 11 2 11 22 ......"
+ *  每组中 1 或者 2 的出现次数分别是 "1 2 2 1 1 2 1 2 2 1 2 2 ......"
+ *  上面的出现次数正是 s 自身
+ *
+ * 给定一个整数 n,返回在神奇字符串 s 的前 n 个数字中 1 的数目
+ *
+ * 1 <= n <= 10^5
+ *
+ * @author Li.zongjie
+ * @version 1.0
+ * @date 2022/10/31
+ */
+public class Z1AmazingString {
+
+    public int magicalString(int n) {
+        int result = 0;
+        int current = 1;
+        LinkedList<Integer> values = new LinkedList<>();
+        while (n > 0) {
+            values.addLast(current);
+            Integer number = values.pollFirst();
+            if (number == 2) {
+                values.addLast(current);
+            }
+            n -= number;
+            if (current == 1) {
+                result += number;
+                if (n < 0) {
+                    result--;
+                }
+            }
+            current ^= 3;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Z1AmazingString test = new Z1AmazingString();
+        // 3
+        System.out.println(test.magicalString(6));
+    }
+}
