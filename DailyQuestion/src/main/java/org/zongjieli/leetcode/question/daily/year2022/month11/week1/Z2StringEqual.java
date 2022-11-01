c9a84a28af376146feb90bba25d0807cfee42fdb
li·zongjie <164260294@qq.com>
2022-11-01 10:43:20 +0800

    IMP 优化判断两个字符串数组拼接后的字符串是否相等的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month11/week1/Z2StringEqual.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month11/week1/Z2StringEqual.java
@@ -16,20 +16,14 @@ package org.zongjieli.leetcode.question.daily.year2022.month11.week1;
 public class Z2StringEqual {
 
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
-        int wi1 = 0, wi2 = 0, wii1 = 0, wii2 = 0;
-        while (wi1 < word1.length && wi2 < word2.length) {
-            if (word1[wi1].charAt(wii1++) != word2[wi2].charAt(wii2++)) {
-                return false;
-            }
-            if (wii1 == word1[wi1].length()) {
-                wii1 = 0;
-                wi1++;
-            }
-            if (wii2 == word2[wi2].length()) {
-                wii2 = 0;
-                wi2++;
-            }
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for (String s : word1) {
+            sb1.append(s);
         }
-        return wi1 == word1.length && wi2 == word2.length;
+        for (String s : word2) {
+            sb2.append(s);
+        }
+        return sb1.toString().equals(sb2.toString());
     }
 }
