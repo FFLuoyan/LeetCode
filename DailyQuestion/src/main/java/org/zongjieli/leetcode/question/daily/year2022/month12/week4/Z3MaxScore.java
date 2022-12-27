e42676bb7fe91e6b3296b77a63c79ece5a6da569
li·zongjie <164260294@qq.com>
2022-12-27 15:33:58 +0800

    ADD 完成计算移除石子的最大得分的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month12/week4/Z3MaxScore.java
@@ -0,0 +1,30 @@
+package org.zongjieli.leetcode.question.daily.year2022.month12.week4;
+
+/**
+ * 一个单人游戏,面前放置着大小分别为 a、b 和 c 的 三堆石子
+ * 每回合都要从两个不同的非空堆中取出一个石子
+ * 并在得分上加 1 分,当存在两个或更多的空堆时,游戏停止
+ * 给定三个整数 a 、b 和 c,返回可以得到的最大分数
+ *
+ * 1 <= a, b, c <= 10^5
+ *
+ * @author Li.zongjie
+ * @version 1.0
+ * @date 2022/12/27
+ */
+public class Z3MaxScore {
+
+    public int maximumScore(int a, int b, int c) {
+        if (a < b) {
+            return maximumScore(b, a, c);
+        }
+        if (b < c) {
+            return maximumScore(a, c, b);
+        }
+        // a >= b, b >= c
+        if (a >= b + c) {
+            return b + c;
+        }
+        return (a + b + c) >> 1;
+    }
+}
