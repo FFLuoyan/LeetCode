5bdf8522e9cd7a86b5c621a16aacd81a55834a92
li·zongjie <164260294@qq.com>
2023-01-11 17:55:08 +0800

    IMP 优化计算使数组递增的最小操作数的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month12/week2/Z7IncreaseArray.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month12/week2/Z7IncreaseArray.java
@@ -18,12 +18,14 @@ package org.zongjieli.leetcode.question.daily.year2022.month12.week2;
 public class Z7IncreaseArray {
 
     public int minOperations(int[] nums) {
-        int result = 0;
+        int result = 0, before = nums[0] + 1;
         for (int i = 1 ; i < nums.length ; i++) {
-            if (nums[i] <= nums[i - 1]) {
-                result += (nums[i - 1] - nums[i] + 1);
-                nums[i] = nums[i - 1] + 1;
+            int current = nums[i];
+            if (current < before) {
+                result += (before - current);
+                current = before;
             }
+            before = current + 1;
         }
         return result;
     }
