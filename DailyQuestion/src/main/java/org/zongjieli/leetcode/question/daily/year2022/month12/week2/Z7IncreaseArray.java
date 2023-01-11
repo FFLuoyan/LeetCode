2392857aef7c0ec3f7b09d9ebd75da44dba63f41
li·zongjie <164260294@qq.com>
2023-01-11 17:43:02 +0800

    ADD 完成计算使数组递增的最小操作数的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month12/week2/Z7IncreaseArray.java
@@ -0,0 +1,31 @@
+package org.zongjieli.leetcode.question.daily.year2022.month12.week2;
+
+/**
+ * 给定一个整数数组 nums(下标从 0 开始)
+ * 每一次操作中,可以选择数组中一个元素,并将它增加 1
+ * 比方说,如果 nums = [1,2,3],可以选择增加 nums[1] 得到 nums = [1,3,3]
+ * 请返回使 nums 严格递增的最少操作次数
+ * 我们称数组 nums 是严格递增的,当它满足对于所有的 0 <= i < nums.length - 1 都有 nums[i] < nums[i+1]
+ * 一个长度为 1 的数组是严格递增的一种特殊情况
+ *
+ * 1 <= nums.length <= 5000
+ * 1 <= nums[i] <= 104
+ *
+ * @author Li.zongjie
+ * @version 1.0
+ * @date 2023/1/11
+ */
+public class Z7IncreaseArray {
+
+    public int minOperations(int[] nums) {
+        int result = 0;
+        for (int i = 1 ; i < nums.length ; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                result += (nums[i - 1] - nums[i] + 1);
+                nums[i] = nums[i - 1] + 1;
+            }
+        }
+        return result;
+    }
+
+}
