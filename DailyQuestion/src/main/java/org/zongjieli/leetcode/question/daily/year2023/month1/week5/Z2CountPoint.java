763ca3a570e39ebeea73a21866cda40e7fa18787
li·zongjie <164260294@qq.com>
2023-01-30 13:54:57 +0800

    IMP 优化计算圆内点的数目的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month1/week5/Z2CountPoint.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month1/week5/Z2CountPoint.java
@@ -25,12 +25,15 @@ package org.zongjieli.leetcode.question.daily.year2023.month1.week5;
 public class Z2CountPoint {
 
     public int[] countPoints(int[][] points, int[][] queries) {
+        for (int[] query : queries) {
+            query[2] *= query[2];
+        }
         int[] answer = new int[queries.length];
         for (int[] point : points) {
             for (int i = 0; i < queries.length; i++) {
                 int xv = point[0] - queries[i][0];
                 int yv = point[1] - queries[i][1];
-                if (xv * xv + yv * yv <= queries[i][2] * queries[i][2]) {
+                if (xv * xv + yv * yv <= queries[i][2]) {
                     answer[i]++;
                 }
             }
