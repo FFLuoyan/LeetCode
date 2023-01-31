67df65738efcf848adb47a778b2e8b2737e205f1
li·zongjie <164260294@qq.com>
2023-01-31 09:15:08 +0800

    ADD 完成判断矩阵是否为 X 型矩阵的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month1/week6/Z2CheckXMatrix.java
@@ -0,0 +1,31 @@
+package org.zongjieli.leetcode.question.daily.year2023.month1.week6;
+
+/**
+ * 如果一个正方形矩阵满足下述全部条件,则称之为一个 X 矩阵:
+ *  矩阵对角线上的所有元素都不是 0
+ *  矩阵中所有其他元素都是 0
+ *  给定一个大小为 n x n 的二维整数数组 grid 表示一个正方形矩阵
+ *  如果 grid 是一个 X 矩阵,返回 true,否则返回 false
+ *
+ * n == grid.length == grid[i].length
+ * 3 <= n <= 100
+ * 0 <= grid[i][j] <= 10^5
+ *
+ * @author Li.zongjie
+ * @version 1.0
+ * @date 2023/1/31
+ */
+public class Z2CheckXMatrix {
+
+    public boolean checkXMatrix(int[][] grid) {
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if ((i == j || grid.length - 1 - j == i) ^ grid[i][j] != 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
