ff856e35ccd42a1bc20379cad9acf057796e7da8
li·zongjie <164260294@qq.com>
2023-02-01 22:33:23 +0800

    IMP 优化计算最大子数组平均和的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month11/week5/Z1MaxAvgSubArray.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month11/week5/Z1MaxAvgSubArray.java
@@ -17,25 +17,24 @@ package org.zongjieli.leetcode.question.daily.year2022.month11.week5;
 public class Z1MaxAvgSubArray {
 
     public double largestSumOfAverages(int[] nums, int k) {
-        double[][] save = new double[nums.length][k + 1];
+        int length = nums.length, sum = nums[length - 1];
+        double[][] save = new double[length][k + 1];
+        save[length - 1][1] = sum;
+        for (int i = length - 2; i >= k - 1; i--) {
+            save[i][1] = (sum += nums[i]) / (length - i + 0d);
+        }
+        if (k == 1) {
+            return save[0][1];
+        }
+        for (int i = 2; i < k; i++) {
+            save[length - i][i] = save[length - i + 1][i - 1] + nums[length - i];
+        }
         return largestSumOfAverages(nums, k, 0, save);
     }
 
     public double largestSumOfAverages(int[] nums, int k, int index, double[][] save) {
         int sum = 0;
-        if (k == 1) {
-            for (int i = index ; i < nums.length ; i++) {
-                sum += nums[i];
-            }
-            return save[index][k] = 1d * sum / (nums.length - index);
-        }
         double result = 0d;
-        if (nums.length - index == k) {
-            for (int i = index ; i < nums.length ; i++) {
-                sum += nums[i];
-            }
-            return save[index][k] = sum;
-        }
         for (int i = index ; i <= nums.length - k; i++) {
             double next = save[i + 1][k - 1] > 0 ? save[i + 1][k - 1] : largestSumOfAverages(nums, k - 1, i + 1, save);
             result = Math.max(result, (sum += nums[i]) / (i - index + 1d) + next);
@@ -46,6 +45,12 @@ public class Z1MaxAvgSubArray {
     public static void main(String[] args) {
         Z1MaxAvgSubArray test = new Z1MaxAvgSubArray();
         // 20
-        System.out.println(test.largestSumOfAverages(new int[]{9, 1, 2, 3, 9}, 3));
+//        System.out.println(test.largestSumOfAverages(new int[]{9, 1, 2, 3, 9}, 3));
+//        // 20.5
+//        System.out.println(test.largestSumOfAverages(new int[]{1, 2, 3, 4, 5, 6, 7}, 4));
+//        // 1
+//        System.out.println(test.largestSumOfAverages(new int[]{1}, 1));
+        // 3
+        System.out.println(test.largestSumOfAverages(new int[]{1, 2}, 1));
     }
 }
