6b381786fe12d72bd8b3520523fe37fbae67d844
li·zongjie <164260294@qq.com>
2023-02-03 15:56:10 +0800

    ADD 完成计算匹配的子序列的数目的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month11/week3/Z4MatchSubstring.java
@@ -0,0 +1,43 @@
+package org.zongjieli.leetcode.question.daily.year2022.month11.week3;
+
+/**
+ * 给定字符串 s 和字符串数组 words,返回 words[i] 中是 s 的子序列的单词个数
+ * 字符串的子序列是从原始字符串中生成的新字符串
+ * 可以从中删去一些字符(可以是 none),而不改变其余字符的相对顺序
+ * 例如, "ace" 是 "abcde" 的子序列
+ *
+ * 1 <= s.length <= 5 * 10^4
+ * 1 <= words.length <= 5000
+ * 1 <= words[i].length <= 50
+ * words[i]和 s 都只由小写字母组成
+ *
+ * @author Li.zongjie
+ * @version 1.0
+ * @date 2023/2/3
+ */
+public class Z4MatchSubstring {
+
+    public int numMatchingSubseq(String s, String[] words) {
+        int result = 0;
+        for (String word : words) {
+            int findIndex = 0;
+            for (char c : word.toCharArray()) {
+                if ((findIndex = s.indexOf(c, findIndex)) == -1) {
+                    break;
+                }
+                findIndex++;
+            }
+            if (findIndex != -1) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Z4MatchSubstring test = new Z4MatchSubstring();
+        // 3
+        System.out.println(test.numMatchingSubseq("abcde", new String[]{"a", "bb", "acd", "ace"}));
+    }
+
+}
