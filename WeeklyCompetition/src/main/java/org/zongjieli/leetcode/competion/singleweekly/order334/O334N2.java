c8485f55914ed3a0295502ec57b3268e9a4a93a2
li·zongjie <164260294@qq.com>
2023-02-27 09:25:53 +0800

    ADD 完成单周赛 333 期第二题,计算字符串的可整除数组的算法

---
+++ WeeklyCompetition/src/main/java/org/zongjieli/leetcode/competion/singleweekly/order334/O334N2.java
@@ -0,0 +1,33 @@
+package org.zongjieli.leetcode.competion.singleweekly.order334;
+
+/**
+ * 给定一个下标从 0 开始的字符串 word,长度为 n,由从 0 到 9 的数字组成,另给定一个正整数 m
+ * word 的可整除数组 div 是一个长度为 n 的整数数组,并满足:
+ *  如果 word[0,...,i] 所表示的数值能被 m 整除,div[i] = 1,否则 div[i] = 0
+ * 返回 word 的可整除数组
+ *
+ * 1 <= n <= 10^5
+ * word.length == n
+ * word 由数字 0 到 9 组成
+ * 1 <= m <= 10^9
+ *
+ * @author Li.zongjie
+ * @version 1.0
+ * @date 2023/2/26
+ */
+public class O334N2 {
+
+    public int[] divisibilityArray(String word, int m) {
+        int[] result = new int[word.length()];
+        long value = 0;
+        for (int i = 0 ; i < word.length() ; i++) {
+            value = value * 10 + (word.charAt(i) - '0');
+            value = value % m;
+            if (value == 0) {
+                result[i] = 1;
+            }
+        }
+        return result;
+    }
+
+}
