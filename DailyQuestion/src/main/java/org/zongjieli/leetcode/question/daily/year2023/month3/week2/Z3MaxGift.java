2fee1b3e93eab9e07fa5a8182518dff7f24c03a0
li·zongjie <164260294@qq.com>
2023-03-08 11:30:26 +0800

    ADD 完成计算可获取礼物最大值的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month3/week2/Z3MaxGift.java
@@ -0,0 +1,41 @@
+package org.zongjieli.leetcode.question.daily.year2023.month3.week2;
+
+/**
+ * 在一个 m*n 的棋盘的每一格都放有一个礼物
+ * 每个礼物都有一定的价值(价值大于 0)
+ * 可以从棋盘的左上角开始拿格子里的礼物
+ * 每次向右或者向下移动一格、直到到达棋盘的右下角
+ * 给定一个棋盘及其上面的礼物的价值
+ * 请计算最多能拿到多少价值的礼物
+ *
+ * 0 < grid.length <= 200
+ * 0 < grid[0].length <= 200
+ *
+ * @author Li.zongjie
+ * @version 1.0
+ * @date 2023/3/8
+ */
+public class Z3MaxGift {
+
+    public int maxValue(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[][] max = new int[m][n];
+        return maxValue(grid, max, m - 1, n - 1);
+    }
+
+    public int maxValue(int[][] grid, int[][] max, int i, int j) {
+        if (max[i][j] != 0) {
+            return max[i][j];
+        }
+        if (i == 0) {
+            if (j == 0) {
+                return grid[0][0];
+            }
+            return max[i][j] = maxValue(grid, max, i, j - 1) + grid[i][j];
+        } else if (j == 0) {
+            return max[i][j] = maxValue(grid, max, i - 1, j) + grid[i][j];
+        } else {
+            return max[i][j] = Math.max(maxValue(grid, max, i - 1, j), maxValue(grid, max, i, j - 1)) + grid[i][j];
+        }
+    }
+}
