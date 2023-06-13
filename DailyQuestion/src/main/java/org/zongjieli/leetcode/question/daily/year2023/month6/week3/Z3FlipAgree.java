11d4a7092312af5b7727f0cd5b699dc02a98d074
li·zongjie <164260294@qq.com>
2023-06-14 00:47:16 +0800

    IMP 优化计算字符串前缀一致次数的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month6/week3/Z3FlipAgree.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month6/week3/Z3FlipAgree.java
@@ -21,7 +21,9 @@ public class Z3FlipAgree {
     public int numTimesAllBlue(int[] flips) {
         int result = 0, max = 0;
         for (int i = 0; i < flips.length; i++) {
-            max = Math.max(max, flips[i]);
+            if (flips[i] > max) {
+                max = flips[i];
+            }
             if (max == i + 1) {
                 result++;
             }
