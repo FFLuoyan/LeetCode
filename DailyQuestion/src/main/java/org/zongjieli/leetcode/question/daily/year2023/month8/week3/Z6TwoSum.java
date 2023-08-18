cc5f7a93dd4935f70b3fa5dc3a9b1092da574dfd
li·zongjie <164260294@qq.com>
2023-08-19 01:36:10 +0800

    ADD 完成计算两个整数的和的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month8/week3/Z6TwoSum.java
@@ -0,0 +1,18 @@
+package org.zongjieli.leetcode.question.daily.year2023.month8.week3;
+
+/**
+ * 给定两个整数 num1 和 num2,返回这两个整数的和
+ *
+ * -100 <= num1, num2 <= 100
+ *
+ * @author Li.zongjie
+ * @version 1.0
+ * @date 2023/8/19
+ */
+public class Z6TwoSum {
+
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+}
