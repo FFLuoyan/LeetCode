23adcfe133ba13969956d29198d1bdb2e779b48c
li·zongjie <164260294@qq.com>
2023-08-26 14:57:24 +0800

    ADD 完成计算有序数组所属区间列表的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month8/week4/Z6RangeList.java
@@ -0,0 +1,43 @@
+package org.zongjieli.leetcode.question.daily.year2023.month8.week4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个无重复元素的有序整数数组 nums
+ * 返回恰好覆盖数组中所有数字的最小有序区间范围列表
+ * 也就是说,nums 的每个元素都恰好被某个区间范围所覆盖
+ * 并且不存在属于某个范围但不属于 nums 的数字 x
+ * 列表中的每个区间范围 [a,b] 应该按如下格式输出:
+ *  "a->b",如果 a != b
+ *  "a",如果 a == b
+ *
+ *  0 <= nums.length <= 20
+ * -2^31 <= nums[i] <= 2^31 - 1
+ * nums 中的所有值都互不相同
+ * nums 按升序排列
+ *
+ * @author Li.zongjie
+ * @version 1.0
+ * @date 2023/8/26
+ */
+public class Z6RangeList {
+
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>(nums.length);
+        if (nums.length == 0) {
+            return result;
+        }
+        int start = 0, end = start;
+        while (++end < nums.length) {
+            if (nums[end] - nums[end - 1] != 1) {
+                String first = String.valueOf(nums[start]);
+                result.add(end - 1 == start ? first : first + "->" + nums[end - 1]);
+                start = end;
+            }
+        }
+        String first = String.valueOf(nums[start]);
+        result.add(--end == start ? first : first + "->" + nums[end]);
+        return result;
+    }
+}
