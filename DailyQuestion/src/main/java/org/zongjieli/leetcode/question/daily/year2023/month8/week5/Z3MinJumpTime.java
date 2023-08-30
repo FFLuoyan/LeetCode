040badbe0dd19894a43e284f31b0c7ad0b3b4655
li·zongjie <164260294@qq.com>
2023-08-30 14:25:26 +0800

    IMP 优化计算最小跳跃次数的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month8/week5/Z3MinJumpTime.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month8/week5/Z3MinJumpTime.java
@@ -32,7 +32,7 @@ public class Z3MinJumpTime {
         if (x % common != 0) {
             return -1;
         }
-        int[] position = new int[6001];
+        int[] position = new int[2000 + a + b];
         for (int i : forbidden) {
             position[i] = -1;
         }
@@ -64,7 +64,7 @@ public class Z3MinJumpTime {
         Z3MinJumpTime test = new Z3MinJumpTime();
         // 121
         System.out.println(test.minimumJumps(new int[]{162, 118, 178, 152, 167, 100, 40, 74, 199, 186, 26, 73, 200, 127, 30, 124, 193, 84, 184, 36, 103, 149, 153, 9, 54, 154, 133, 95, 45, 198, 79, 157, 64, 122, 59, 71, 48, 177, 82, 35, 14, 176, 16, 108, 111, 6, 168, 31, 134, 164, 136, 72, 98}, 29, 98, 80));
-        //
+        // 3998
         System.out.println(test.minimumJumps(new int[]{1998}, 1999, 2000, 2000));
     }
 }
