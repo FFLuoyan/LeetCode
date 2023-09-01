63d72978610bb14e87595997faad74ef043b5be2
li·zongjie <164260294@qq.com>
2023-09-01 10:37:08 +0800

    ADD 完成计算购买方案的不同数目的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month9/week1/Z5CountPlans.java
@@ -0,0 +1,25 @@
+package org.zongjieli.leetcode.question.daily.year2023.month9.week1;
+
+/**
+ * 给定一个整数 total,表示拥有的总钱数
+ * 同时给定两个整数 cost1 和 cost2, 分别表示一支钢笔和一支铅笔的价格
+ * 可以花费部分或者全部的钱,去买任意数目的两种笔
+ * 请返回购买钢笔和铅笔的不同方案数目
+ *
+ * 1 <= total, cost1, cost2 <= 10^6
+ *
+ * @author Li.zongjie
+ * @version 1.0
+ * @date 2023/9/1
+ */
+public class Z5CountPlans {
+
+    public long waysToBuyPensPencils(int total, int cost1, int cost2) {
+        long result = 0;
+        for (int i = 0 ; i <= total ; i += cost1) {
+            result += (total - i) / cost2 + 1;
+        }
+        return result;
+    }
+
+}
