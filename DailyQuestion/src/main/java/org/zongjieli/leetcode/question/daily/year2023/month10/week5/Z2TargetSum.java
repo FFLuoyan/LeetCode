72b3cfe96bdbae8abf0b4849fe286112182ce8c1
lizongjie <lizongjie@powerlaw.ai>
2023-10-24 15:19:17 +0800

    IMP 优化计算和为指定值的排列组合方式的总数的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month10/week5/Z2TargetSum.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month10/week5/Z2TargetSum.java
@@ -15,22 +15,46 @@ package org.zongjieli.leetcode.question.daily.year2023.month10.week5;
  */
 public class Z2TargetSum {
 
+    int k;
+
+    boolean[][] exist;
+
+    int[][] values;
+
     public int numRollsToTarget(int n, int k, int target) {
-        if (n * k < target) {
+        this.k = k;
+        int max = n * k;
+        if (max < target) {
             return 0;
         }
-        int[] values = new int[target + 1];
-        values[0] = 1;
-        while (n-- > 0) {
-            int[] next = new int[target + 1];
-            for (int i = 1; i <= k; i++) {
-                for (int j = target - i ; j >= 0 ; j--) {
-                    next[i + j] = (next[i + j] + values[j]) % 1000000007;
-                }
-            }
-            values = next;
+        exist = new boolean[n + 1][++max];
+        values = new int[n + 1][max];
+        for (int i = 2 ; i < k ; i++) {
+            exist[1][i] = true;
+            values[1][i] = 1;
         }
-        return values[target];
+        for (int i = 1 ; i <= n ; i++) {
+            exist[i][i] = true;
+            exist[i][i * k] = true;
+            values[i][i] = 1;
+            values[i][i * k] = 1;
+            for (int j = i * k + 1 ; j < max ; j++) {
+                exist[i][j] = true;
+            }
+        }
+        return count(n, target);
+    }
+
+    public int count(int n, int target) {
+        if (exist[n][target]) {
+            return values[n][target];
+        }
+        exist[n][target] = true;
+        long result = 0;
+        for (int current = Math.min(target - n + 1, k) ; current >= 1 ; current--) {
+            result += count(n - 1, target - current);
+        }
+        return values[n][target] = (int) (result % 1000000007);
     }
 
     public static void main(String[] args) {
