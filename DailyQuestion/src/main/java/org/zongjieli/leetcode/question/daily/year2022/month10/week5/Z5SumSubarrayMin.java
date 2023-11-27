f41e3bd08d15cffed3e660a26ba8a04d6ed8c6de
lizongjie <lizongjie@powerlaw.ai>
2023-11-27 23:20:39 +0800

    IMP 优化项目结构

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month10/week5/Z5SumSubarrayMin.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2022/month10/week5/Z5SumSubarrayMin.java
@@ -1,12 +1,9 @@
 package org.zongjieli.leetcode.question.daily.year2022.month10.week5;
 
+import org.zongjieli.leetcode.origin.year2023.SubarrayMinSum;
+
 /**
- * 给定一个整数数组 arr,找到 min(b) 的总和
- * 其中 b 的范围为 arr 的每个(连续)子数组
- * 由于答案可能很大,因此返回答案模 10^9 + 7
- *
- * 1 <= arr.length <= 3 * 10^4
- * 1 <= arr[i] <= 3 * 10^4
+ * @see SubarrayMinSum
  *
  * @author Li.zongjie
  * @version 1.0
@@ -15,20 +12,7 @@ package org.zongjieli.leetcode.question.daily.year2022.month10.week5;
 public class Z5SumSubarrayMin {
 
     public int sumSubarrayMins(int[] arr) {
-        long result = 0;
-        for (int i = 0 ; i < arr.length ; i++) {
-            int before, after, value = arr[i];
-            for (before = i - 1 ; before >= 0 && arr[before] >= value ; before--) {}
-            for (after = i + 1 ; after < arr.length && arr[after] > value ; after++) {}
-            result = result + (long) (i - before) * (after - i) * value;
-        }
-        return (int) (result % 1000000007);
-    }
-
-    public static void main(String[] args) {
-        Z5SumSubarrayMin test = new Z5SumSubarrayMin();
-        // 3
-        System.out.println(test.sumSubarrayMins(new int[]{1, 1}));
+        return new SubarrayMinSum().sumSubarrayMins(arr);
     }
 
 }
--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month11/week5/Z1SumMin.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month11/week5/Z1SumMin.java
@@ -1,12 +1,9 @@
 package org.zongjieli.leetcode.question.daily.year2023.month11.week5;
 
+import org.zongjieli.leetcode.origin.year2023.SubarrayMinSum;
+
 /**
- * 给定一个整数数组 arr,找到 min(b) 的总和
- * 其中 b 的范围为 arr 的每个(连续)子数组
- * 由于答案可能很大,因此返回答案模 10^9 + 7
- *
- * 1 <= arr.length <= 3 * 10^4
- * 1 <= arr[i] <= 3 * 10^4
+ * @see SubarrayMinSum
  *
  * @author Li.zongjie
  * @version 1.0
@@ -15,24 +12,7 @@ package org.zongjieli.leetcode.question.daily.year2023.month11.week5;
 public class Z1SumMin {
 
     public int sumSubarrayMins(int[] arr) {
-        long result = 0;
-        for (int i = 0; i < arr.length; i++) {
-            int left = i - 1, right = i + 1;
-            long value = arr[i];
-            while (left >= 0 && arr[left] >= value) {
-                left--;
-            }
-            while (right < arr.length && arr[right] > value) {
-                right++;
-            }
-            result = (result + value * (i - left) * (right - i)) % 1000000007;
-        }
-        return (int) result;
+        return new SubarrayMinSum().sumSubarrayMins(arr);
     }
 
-    public static void main(String[] args) {
-        Z1SumMin test = new Z1SumMin();
-        // 444
-        System.out.println(test.sumSubarrayMins(new int[]{11, 81, 94, 43, 3}));
-    }
 }
