211c27d06e1c1361a52562b788ab5d6bc8d139c9
lizongjie <lizongjie@powerlaw.ai>
2023-11-27 23:16:08 +0800

    ADD 完成计算数组所有子数组的最小值的和的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month11/week5/Z1SumMin.java
@@ -0,0 +1,38 @@
+package org.zongjieli.leetcode.question.daily.year2023.month11.week5;
+
+/**
+ * 给定一个整数数组 arr,找到 min(b) 的总和
+ * 其中 b 的范围为 arr 的每个(连续)子数组
+ * 由于答案可能很大,因此返回答案模 10^9 + 7
+ *
+ * 1 <= arr.length <= 3 * 10^4
+ * 1 <= arr[i] <= 3 * 10^4
+ *
+ * @author Li.zongjie
+ * @version 1.0
+ * @date 2023/11/27
+ */
+public class Z1SumMin {
+
+    public int sumSubarrayMins(int[] arr) {
+        long result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int left = i - 1, right = i + 1;
+            long value = arr[i];
+            while (left >= 0 && arr[left] >= value) {
+                left--;
+            }
+            while (right < arr.length && arr[right] > value) {
+                right++;
+            }
+            result = (result + value * (i - left) * (right - i)) % 1000000007;
+        }
+        return (int) result;
+    }
+
+    public static void main(String[] args) {
+        Z1SumMin test = new Z1SumMin();
+        // 444
+        System.out.println(test.sumSubarrayMins(new int[]{11, 81, 94, 43, 3}));
+    }
+}
