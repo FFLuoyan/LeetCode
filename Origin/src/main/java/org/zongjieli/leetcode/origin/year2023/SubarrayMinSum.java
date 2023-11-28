cf510a275b131ea26481e10b7780b0a59993a338
lizongjie <lizongjie@powerlaw.ai>
2023-11-28 11:28:07 +0800

    IMP 优化计算数组所有子数组的最小值的和的算法

--- Origin/src/main/java/org/zongjieli/leetcode/origin/year2023/SubarrayMinSum.java
+++ Origin/src/main/java/org/zongjieli/leetcode/origin/year2023/SubarrayMinSum.java
@@ -1,6 +1,5 @@
 package org.zongjieli.leetcode.origin.year2023;
 
-
 /**
  * 给定一个整数数组 arr,找到 min(b) 的总和
  * 其中 b 的范围为 arr 的每个(连续)子数组
@@ -17,18 +16,32 @@ public class SubarrayMinSum {
 
     public int sumSubarrayMins(int[] arr) {
         long result = 0;
+        int[] leftCount = new int[arr.length];
+        int[] nearestLess = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
-            int left = i - 1, right = i + 1;
-            long value = arr[i];
-            while (left >= 0 && arr[left] >= value) {
-                left--;
-            }
-            while (right < arr.length && arr[right] > value) {
-                right++;
-            }
-            result = (result + value * (i - left) * (right - i)) % 1000000007;
+            leftCount[i] = findBeforeCount(i, nearestLess, arr);
         }
-        return (int) result;
+        for (int i = arr.length - 1; i >= 0; i--) {
+            long rightCount = findAfterCount(i, nearestLess, arr);
+            result += (rightCount * leftCount[i]) * arr[i];
+        }
+        return (int) (result % 1000000007);
+    }
+
+    public int findBeforeCount(int currentIndex, int[] nearestLess, int[] arr) {
+        int value = arr[currentIndex], index = currentIndex - 1;
+        while (index > -1 && arr[index] >= value) {
+            index = nearestLess[index];
+        }
+        return currentIndex - (nearestLess[currentIndex] = index);
+    }
+
+    public int findAfterCount(int currentIndex, int[] nearestLess, int[] arr) {
+        int value = arr[currentIndex], index = currentIndex + 1;
+        while (index < arr.length && arr[index] > value) {
+            index = nearestLess[index];
+        }
+        return (nearestLess[currentIndex] = index) - currentIndex;
     }
 
     public static void main(String[] args) {
@@ -37,6 +50,8 @@ public class SubarrayMinSum {
         System.out.println(test.sumSubarrayMins(new int[]{11, 81, 94, 43, 3}));
         // 3
         System.out.println(test.sumSubarrayMins(new int[]{1, 1}));
+        // 17
+        System.out.println(test.sumSubarrayMins(new int[]{3, 1, 2, 4}));
     }
 
 }
