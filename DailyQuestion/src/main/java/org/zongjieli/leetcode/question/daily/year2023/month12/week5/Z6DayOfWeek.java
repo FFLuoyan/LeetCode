9d19161704becd51d6ee021b4633e0910ad7447d
lizongjie <lizongjie@powerlaw.ai>
2023-12-31 19:12:11 +0800

    IMP 优化计算日期为一星期的第几天的算法

--- DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month12/week5/Z6DayOfWeek.java
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2023/month12/week5/Z6DayOfWeek.java
@@ -1,7 +1,5 @@
 package org.zongjieli.leetcode.question.daily.year2023.month12.week5;
 
-import java.util.Calendar;
-
 /**
  * 给定一个日期,请设计一个算法来判断它是对应一周中的哪一天
  * 输入为三个整数: day、month 和 year,分别表示日、月、年
@@ -15,15 +13,28 @@ public class Z6DayOfWeek {
 
     private final String[] days = new String[]{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
+    private final int[] monthDayCount = new int[]{0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334};
     public String dayOfTheWeek(int day, int month, int year) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(year, month - 1, day);
-        return days[calendar.get(Calendar.DAY_OF_WEEK) - 1];
+        int yearCount = year - 1971;
+        int dayCountYear = ((yearCount + 2) / 4) * 366 + (yearCount - (yearCount + 2) / 4) *  365;
+        int dayCountMonth = monthDayCount[month - 1] + (month > 2 && isLeapYear(year) ? 1 : 0);
+        int dayCountDay = day - 1;
+        return days[(dayCountYear + dayCountMonth + dayCountDay + 5) % 7];
+    }
+
+    public boolean isLeapYear(int year) {
+        return year % 100 == 0 ? year % 400 == 0 : year % 4 == 0;
     }
 
     public static void main(String[] args) {
         Z6DayOfWeek test = new Z6DayOfWeek();
         // Sunday
         System.out.println(test.dayOfTheWeek(31, 12, 2023));
+        // Sunday
+        System.out.println(test.dayOfTheWeek(15, 8, 1993));
+        // Friday
+        System.out.println(test.dayOfTheWeek(1, 1, 1993));
+        // Friday
+        System.out.println(test.dayOfTheWeek(1, 1, 1971));
     }
 }
