53435b6459480f369400146f373c51d4aef5d1a6
lizongjie <lizongjie@powerlaw.ai>
2024-01-24 19:01:09 +0800

    ADD 完成在链表中插入相邻节点最大公约数的算法

---
+++ DailyQuestion/src/main/java/org/zongjieli/leetcode/question/daily/year2024/month1/week1/Z6InsertCommon.java
@@ -0,0 +1,41 @@
+package org.zongjieli.leetcode.question.daily.year2024.month1.week1;
+
+import org.zongjieli.leetcode.base.ListNode;
+
+/**
+ * 给定一个链表的头 head,每个结点包含一个整数值
+ * 在相邻结点之间,请插入一个新的结点,结点值为这两个相邻结点值的最大公约数
+ * 请返回插入之后的链表
+ * 两个数的最大公约数是可以被两个数字整除的最大正整数
+ *
+ * 链表中结点数目在 [1, 5000] 之间
+ * 1 <= Node.val <= 1000
+ *
+ * @author Li.zongjie
+ * @version 1.0
+ * @date 2024/1/24
+ */
+public class Z6InsertCommon {
+
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        ListNode pre = head, next = pre.next;
+        while (next != null) {
+            int common = common(pre.val, next.val);
+            pre.next = new ListNode(common, next);
+            pre = next;
+            next = next.next;
+        }
+        return head;
+    }
+
+    public int common(int a, int b) {
+        if (b > a) {
+            return common(b, a);
+        }
+        if (a % b == 0) {
+            return b;
+        }
+        return common(b, a % b);
+    }
+
+}
